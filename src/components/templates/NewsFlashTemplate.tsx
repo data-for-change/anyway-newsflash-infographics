8bc6ff05895ed00156e8a498f459b73a99925e81
yuvalbl <yuval.bar.levi@gmail.com>
2019-12-16 21:49:25 +0200

    infra: add mobx minor fix

---
+++ src/components/templates/NewsFlashTemplate.tsx
@@ -0,0 +1,11 @@
+import React, {FunctionComponent} from 'react';
+
+interface IProps {
+}
+
+export const NewsFlashTemplate: FunctionComponent<IProps> = () => {
+
+  return (
+    <div>NewsFlashTemplate</div>
+  );
+};
