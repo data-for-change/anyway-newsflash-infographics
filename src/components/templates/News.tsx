00ff79b3a7a436f86a6e3da1bd768e50f334bb13
danielsh28 <danielshely1@gmail.com>
2019-12-30 19:01:28 +0200

    Added newsflash sidebar-  using locale date string

--- src/components/templates/News.tsx
+++ src/components/templates/News.tsx
@@ -5,8 +5,9 @@ import {Text, TextType} from '../atoms/Text';
 const News: React.FC = () => {
     return (<div>
         {newsApiDummy.map((news) => {
+            const date :Date = new Date(news.date);
             return <div>
-                <Text type={TextType.NEWS_FLASH_TITLE} children={news.date + ', ' + news.source }/>
+                <Text type={TextType.NEWS_FLASH_TITLE} children={ date.toLocaleDateString()+ ', ' + news.source }/>
                 <Text type={TextType.NEWS_FLASH_CONTENT} children={news.title}/>
             </div>
         })}
