ceb26dc90a1bb6dff3f601534f588346ba815f1e
danielsh28 <danielshely1@gmail.com>
2020-01-01 21:43:48 +0200

    added filter button with styles added style to container

--- src/components/atoms/NewsFlahsFilterPanel.tsx
+++ src/components/atoms/NewsFlahsFilterPanel.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ynetLogo from  '../../assets/Ynet_website_logo.svg';
+import wallaLogo from  '../../assets/walla-logo.svg';
 import {Box, makeStyles} from '@material-ui/core';
 import Button from "@material-ui/core/Button";
 
@@ -12,22 +13,25 @@ const useStyles = makeStyles({
     button:{
         Height:'30%',
         maxWidth:'20%',
-        border: '0.5px solid blue',
-        boxShadow: '2px 2px 2px 2px #888888'
+        border: '0.5px solid grey',
+        boxShadow: '1px 1px 1px 1px #888888'
+    },
+    container:{
+      height:'7%'
     }
 });
 
 export const NewsFlashFilterPanel : React.FC = ()=>{
     const classes =useStyles();
     return(
-        <Box  display='flex' flexDirection='row' justifyContent='space-evenly'>
+        <Box  className={classes.container} display='flex' flexDirection='row' justifyContent='space-evenly'>
             <Button  className={classes.button} onClick={() => {
             }}>
                 <img  className={classes.image} src={ynetLogo} alt="Source Logo"/>
             </Button>
             <Button className={classes.button} onClick={() => {
             }}>
-                <img  className={classes.image} src={ynetLogo} alt="Source Logo"/>
+                <img  className={classes.image} src={wallaLogo} alt="Source Logo"/>
             </Button>
             <Button className={classes.button} onClick={() => {
             }}>
