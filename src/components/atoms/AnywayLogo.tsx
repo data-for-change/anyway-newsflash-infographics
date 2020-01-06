a1f7e9fcdc25c2825d564a361e0ebcabf7db0684
Carmel Paradis <16carmel@gmail.com>
2020-01-06 19:47:13 +0200

    Anyway logo component

---
+++ src/components/atoms/AnywayLogo.tsx
@@ -0,0 +1,19 @@
+import React, { FunctionComponent, useMemo } from "react";
+import AnywayImage from "../../assets/anyway.png";
+
+interface AnywayLogoProps {
+  height: string;
+}
+
+export const AnywayLogo: FunctionComponent<AnywayLogoProps> = ({ height }) => {
+  const logo = useMemo(() => AnywayImage.toString(), []);
+
+  const logoStyle = useMemo(
+    () => ({
+      height
+    }),
+    [height]
+  );
+
+  return <img src={logo} alt="logo" style={logoStyle} />;
+};
