8e6243d80f059321af282d3f3dae7cbfbbdb9232
Carmel Paradis <16carmel@gmail.com>
2020-01-06 19:48:09 +0200

    Render anyway header in appbar template

--- src/components/templates/AppBar.tsx
+++ src/components/templates/AppBar.tsx
@@ -1,37 +1,6 @@
-import React, {FunctionComponent} from 'react';
-import logo from '../../logo.svg';
-import {Link} from 'react-router-dom';
+import React, { FunctionComponent } from "react";
+import { AnywayHeader } from "../molecules/AnywayHeader";
 
-interface IProps {
-}
-
-export const AppBar: FunctionComponent<IProps> = () => {
-  const styles = {
-    logo: {
-      height: '15vh'
-    }
-  };
-  
-  return (
-    <div>
-      <header>
-        <h1>AppBar</h1>
-        <img src={logo} alt="logo" style={styles.logo}/>
-        <ul>
-          <li>
-            <Link to="/todo">To do Page</Link>
-          </li>
-          <li>
-            <Link to="/users">Users Page</Link>
-          </li>
-          <li>
-            <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-              Learn React
-            </a>
-          </li>
-        </ul>
-        <hr/>
-      </header>
-    </div>
-  );
+export const AppBar: FunctionComponent = () => {
+  return <AnywayHeader />;
 };
