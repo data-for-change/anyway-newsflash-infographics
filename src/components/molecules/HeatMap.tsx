3d9a289c24dbbe39526498c4ffbc1993596bdc23
Meital Lazarovich <lazarovich.meital@gmail.com>
2020-02-04 16:54:30 +0200

    real app data

--- src/components/molecules/HeatMap.tsx
+++ src/components/molecules/HeatMap.tsx
@@ -6,23 +6,68 @@ import { Map, Marker, TileLayer } from 'react-leaflet';
 import HeatmapLayer from 'react-leaflet-heatmap-layer';
 
 export const geojson = {
-  features: [
-    {
-        coordinates: [39.050619679238508, 55.664990339319459]
-    },
-    {
-        coordinates: [36.804726580056425, 55.327391214040745]
-    },
-    {
-        coordinates: [38.677575326793274, 55.767798037723409]
-    },
-    {
-        coordinates: [38.764793791552123, 55.112493529690383]
-    },
-    {
-        coordinates: [38.287461597995204, 54.758055728752645]
-    }
-  ]
+    data: [
+        {
+            "longitude": 34.9780568296871,
+            "latitude": 32.4543458814189,
+            "accident_severity": "קשה",
+            "accident_timestamp": "2016-05-15 21:00:00"
+        },
+        {
+            "longitude": 34.9618510776681,
+            "latitude": 32.4494011939289,
+            "accident_severity": "קשה",
+            "accident_timestamp": "2015-01-02 06:45:00"
+        },
+        {
+            "longitude": 34.968629300414,
+            "latitude": 32.4520110942299,
+            "accident_severity": "קשה",
+            "accident_timestamp": "2014-09-12 09:45:00"
+        },
+        {
+            "longitude": 34.9618510776681,
+            "latitude": 32.4494011939289,
+            "accident_severity": "קשה",
+            "accident_timestamp": "2014-07-27 21:00:00"
+        },
+        {
+            "longitude": 34.9865860704716,
+            "latitude": 32.458914735012,
+            "accident_severity": "קשה",
+            "accident_timestamp": "2011-03-26 19:30:00"
+        },
+        {
+            "longitude": 34.9865860704716,
+            "latitude": 32.458914735012,
+            "accident_severity": "קלה",
+            "accident_timestamp": "2019-11-28 11:30:00"
+        },
+        {
+            "longitude": 34.9618510776681,
+            "latitude": 32.4494011939289,
+            "accident_severity": "קלה",
+            "accident_timestamp": "2019-10-31 19:45:00"
+        },
+        {
+            "longitude": 34.9625415910224,
+            "latitude": 32.4496820631224,
+            "accident_severity": "קלה",
+            "accident_timestamp": "2019-10-31 06:30:00"
+        },
+        {
+            "longitude": 34.9865860704716,
+            "latitude": 32.458914735012,
+            "accident_severity": "קלה",
+            "accident_timestamp": "2019-10-28 10:30:00"
+        },
+        {
+            "longitude": 34.9618510776681,
+            "latitude": 32.4494011939289,
+            "accident_severity": "קלה",
+            "accident_timestamp": "2019-06-22 12:00:00"
+        }
+    ],
 };
 
 
@@ -43,14 +88,14 @@ const WRAPPER_STYLES = { height: '100%', width: '100%' };
 const LocationMap: FunctionComponent<IProps> = ({ marker }) => {
     return (
         <Map center={marker} zoom={INITIAL_ZOOM} style={WRAPPER_STYLES}>
-          <HeatmapLayer
-            fitBoundsOnLoad
-            fitBoundsOnUpdate
-            points={geojson.features}
-            longitudeExtractor={(m:any) => m.coordinates[0]}
-            latitudeExtractor={(m:any) => m.coordinates[1]}
-            intensityExtractor={(m:any) => parseFloat(m.coordinates[1])}
-          />
+            <HeatmapLayer
+                fitBoundsOnLoad
+                fitBoundsOnUpdate
+                points={geojson.data}
+                longitudeExtractor={(m: any) => m.longitude}
+                latitudeExtractor={(m: any) => m.latitude}
+                intensityExtractor={(m: any) => parseFloat(m.latitude)}
+            />
             <TileLayer
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
