f75040393175cce82526794c05d4744705b9d954
Meital Lazarovich <lazarovich.meital@gmail.com>
2020-02-12 19:58:00 +0200

    heatmap added as extra widget

--- src/components/templates/WidgetsTemplate.tsx
+++ src/components/templates/WidgetsTemplate.tsx
@@ -8,7 +8,7 @@ import AnyWayGrid from '../atoms/AnyWayGrid'
 import AnyWayCard from '../molecules/AnyWayCard'
 import PieChartView from '../molecules/PieChartView'
 import BarChartView from '../molecules/BarChartView'
-// import LocationMap from '../molecules/LocationMap'
+import LocationMap from '../molecules/LocationMap'
 import HeatMap from '../molecules/HeatMap'
 
 interface IProps {}
@@ -20,10 +20,10 @@ const WidgetsTemplate: FunctionComponent<IProps> = observer(() => {
         return (
             <AnyWayGrid>
                 <AnyWayCard key={widget.name}>
-                    {/* {widget.name === 'most_severe_accidents' ? (
-                        <LocationMap key={widget.name} marker={{ lat: 32.0853, lng: 34.7818 }} />
-                    ) : null} */}
                     {widget.name === 'most_severe_accidents' ? (
+                        <LocationMap key={widget.name} marker={{ lat: 32.0853, lng: 34.7818 }} />
+                    ) : null}
+                    {widget.name === 'most_severe_accidents_heatmap' ? (
                         <HeatMap key={widget.name} data={widget.data} marker={{ lat: 32.0853, lng: 34.7818 }} />
                     ) : null}
                     {widget.name === 'accident_count_by_severity' ? (
