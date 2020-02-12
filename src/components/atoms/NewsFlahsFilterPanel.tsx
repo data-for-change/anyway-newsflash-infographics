8f19f94f09d3b22c9bb6e12643cd4ba190a9d92c
danielsh28 <danielshely1@gmail.com>
2020-02-12 20:50:16 +0200

    remove makeStyles outside of component

--- src/components/atoms/NewsFlahsFilterPanel.tsx
+++ src/components/atoms/NewsFlahsFilterPanel.tsx
@@ -4,26 +4,25 @@ import ynetLogo from '../../assets/Ynet_website_logo.svg';
 import wallaLogo from '../../assets/walla-logo.svg';
 import {AnyWayButton} from "./AnyWayButton";
 
-
+const useStyles = makeStyles({
+    image: {
+        height: '70%',
+        width: '70%'
+    },
+    button: {
+        Height: '30%',
+        maxWidth: '20%',
+        border: '0.5px solid grey',
+        boxShadow: '1px 1px 1px 1px #888888'
+    },
+    container: {
+        height: '7%',
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-evenly'
+    }
+});
 export const NewsFlashFilterPanel: React.FC = () => {
-    const useStyles = makeStyles({
-        image: {
-            height: '70%',
-            width: '70%'
-        },
-        button: {
-            Height: '30%',
-            maxWidth: '20%',
-            border: '0.5px solid grey',
-            boxShadow: '1px 1px 1px 1px #888888'
-        },
-        container: {
-            height: '7%',
-            display: 'flex',
-            flexDirection: 'row',
-            justifyContent: 'space-evenly'
-        }
-    });
     const classes = useStyles();
     return (
         // will be convert to filterButtonsMap instead of code duplicate for the filter button
--- src/components/templates/SideBar.tsx
+++ src/components/templates/SideBar.tsx
@@ -5,12 +5,12 @@ import {NewsFlashFilterPanel} from "../atoms/NewsFlahsFilterPanel";
 interface IProps {
 }
 
-export const SideBar: FunctionComponent<IProps> = () => {
-  // remove when no longer required
-  const devStyles = {
+// remove when no longer required
+const devStyles = {
     border: '2px dashed blue',
     height:'100%'
-  };
+};
+export const SideBar: FunctionComponent<IProps> = () => {
 
   return (
       <Box style={devStyles} >
