2b1a878c42077e310d3c9fb327eaadec1c57bd0b
Meital Lazarovich <lazarovich.meital@gmail.com>
2020-02-12 19:17:43 +0200

    new widget template

--- src/components/templates/WidgetsTemplate.tsx
+++ src/components/templates/WidgetsTemplate.tsx
@@ -9,6 +9,7 @@ import AnyWayCard from '../molecules/AnyWayCard'
 import PieChartView from '../molecules/PieChartView'
 import BarChartView from '../molecules/BarChartView'
 import LocationMap from '../molecules/LocationMap'
+import HeatMap from '../molecules/HeatMap'
 
 interface IProps {}
 
@@ -19,8 +20,11 @@ const WidgetsTemplate: FunctionComponent<IProps> = observer(() => {
         return (
             <AnyWayGrid>
                 <AnyWayCard key={widget.name}>
-                    {widget.name === 'most_severe_accidents' ? (
+                    {/* {widget.name === 'most_severe_accidents' ? (
                         <LocationMap key={widget.name} marker={{ lat: 32.0853, lng: 34.7818 }} />
+                    ) : null} */}
+                    {widget.name === 'most_severe_accidents' ? (
+                        <HeatMap key={widget.name} data={widget.data} marker={{ lat: 32.0853, lng: 34.7818 }} />
                     ) : null}
                     {widget.name === 'accident_count_by_severity' ? (
                         <div key={widget.name}>Text</div>
