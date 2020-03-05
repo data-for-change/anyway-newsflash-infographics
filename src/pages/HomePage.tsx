9649e04de7da7a2a966fc4a7457ecbd8cba4798b
Yael Luria <yaeluria@gmail.com>
2020-03-05 21:54:02 +0200

    create About page with boilerplate text, add  Route for about page, update link in footer to lead to the page, move sidebar from App.tsx to HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,20 @@
 import React, { FunctionComponent } from 'react'
 import WidgetsTemplate from '../components/templates/WidgetsTemplate'
+import { Box } from '@material-ui/core'
+import { SideBar } from '../components/templates/SideBar'
 
-interface IProps {}
+interface IProps { }
 
 const HomePage: FunctionComponent<IProps> = () => {
-	return <WidgetsTemplate />
+	return (
+		<div>
+			<Box display='flex' flexGrow={1}>
+				<Box flexGrow={1}>
+					<SideBar />
+				</Box>
+				<WidgetsTemplate />
+			</Box>
+		</div>
+	)
 }
 export default HomePage
