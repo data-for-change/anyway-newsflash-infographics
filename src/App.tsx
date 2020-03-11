84dd37f33266c6326e198b4449849078f6330482
Yael Luria <yaeluria@gmail.com>
2020-03-11 21:02:06 +0200

    Add route for Thank You page, ThankYou page component and link on footer

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import About from './pages/About';
+import ThankYou from './pages/ThankYou';
 import { Footer } from './components/templates/Footer'
 import { Box, CssBaseline } from '@material-ui/core'
 import { store, StoreContext } from './store/storeConfig'
@@ -17,6 +18,7 @@ const App: React.FC = () => {
 						<Switch>
 							<Route exact path='/' component={HomePage} />
 							<Route path='/about' component={About} />
+							<Route path='/thank-you' component={ThankYou} />
 						</Switch>
 					<Footer />
 				</Box>
---
+++ src/pages/ThankYou.tsx
@@ -0,0 +1,18 @@
+import React, { FunctionComponent } from 'react';
+import {Text, TextType} from '../components/atoms';
+
+
+
+interface IProps {}
+
+const ThankYou: FunctionComponent<IProps> = () => {
+    return (
+        <div>
+            <Text type={TextType.PAGE_TITLE}>Thank You</Text>
+            <Text type={TextType.CONTENT_TITLE}>This project was developed by:</Text>
+            <Text type={TextType.CONTENT}>Yuval Bar Lavi, Miki Stanger, Meital Lazarovich, Daniel Shely, Sergey Bekker, Dror Reshef, Adele Angel, Carmel Paradis, Dror Reshef, Atalya Alon</Text>
+        </div>
+    )
+}
+
+export default ThankYou;
