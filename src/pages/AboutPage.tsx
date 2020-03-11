2f25341a32b1df6e950e893a8e98a6fc01bd4442
yuvalbl <yuval.bar.levi@gmail.com>
2020-03-11 17:47:40 +0200

    Hotfix: add about page component file

---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,12 @@
+import React, { FunctionComponent } from 'react'
+interface IProps { }
+
+const AboutPage: FunctionComponent<IProps> = () => {
+	return (
+		<div>
+			AboutPage
+		</div>
+	)
+};
+
+export default AboutPage
