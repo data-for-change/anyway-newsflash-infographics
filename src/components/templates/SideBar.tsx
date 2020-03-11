363deeb22dd68b202375cb73c7367c53c404f949
yuvalbl <yuval.bar.levi@gmail.com>
2020-03-11 21:18:39 +0200

    solve #9

--- src/components/templates/SideBar.tsx
+++ src/components/templates/SideBar.tsx
@@ -2,6 +2,8 @@ import React, {FunctionComponent} from 'react';
 import {Box} from '@material-ui/core';
 import  News from './News';
 import {NewsFlashFilterPanel} from "../atoms/NewsFlahsFilterPanel";
+import LocationMap from '../molecules/LocationMap';
+import {sidBarWidth} from '../../style/_globals';
 interface IProps {
 }
 
@@ -14,8 +16,15 @@ export const SideBar: FunctionComponent<IProps> = () => {
 
   return (
       <Box style={devStyles} >
-          <NewsFlashFilterPanel />
-        <News/>
+        <NewsFlashFilterPanel />
+        <Box display="flex" flexDirection="column">
+          <Box flexGrow="1">
+            <News/>
+          </Box>
+          <Box flexBasis={sidBarWidth} flexShrink={0} height={sidBarWidth} width={sidBarWidth}>
+              <LocationMap marker={{ lat: 32.0853, lng: 34.7818 }} />
+          </Box>
+        </Box>
       </Box>
   );
 };
