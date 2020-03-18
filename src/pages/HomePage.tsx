19565d19b4209a697f9608fbe11730a4f9569e3e
adele-angel <adeler.ang@gmail.com>
2020-03-18 20:44:15 +0200

    fix for horizontal scroll on sidebar

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,39 +1,43 @@
-import React, {FunctionComponent} from 'react'
-import WidgetsTemplate from '../components/templates/WidgetsTemplate'
-import {Box} from '@material-ui/core'
-import {SideBar} from '../components/templates/SideBar'
+import React, { FunctionComponent } from "react";
+import WidgetsTemplate from "../components/templates/WidgetsTemplate";
+import { Box } from "@material-ui/core";
+import { SideBar } from "../components/templates/SideBar";
 import makeStyles from "@material-ui/core/styles/makeStyles";
+import LocationMap from "../components/molecules/LocationMap";
 
-interface IProps {
-}
+interface IProps {}
 
 const useStyles = makeStyles({
-  mainBox: {
-    height: 'inherit'
-  },
-  sideBarBox: {
-    height: 'inherit',
-    overflow: 'auto'
-    
-  },
-  widgetBox: {
-    height: 'inherit',
-    overflow: 'auto'
-  }
+	mainBox: {
+		height: "inherit"
+	},
+	sideBarBox: {
+		height: "inherit",
+		overflow: "auto"
+	},
+	widgetBox: {
+		height: "inherit",
+		overflow: "auto"
+	}
 });
 const HomePage: FunctionComponent<IProps> = () => {
-  const classes = useStyles();
-  
-  return (
-    <Box display='flex' flexGrow={1} className={classes.mainBox}>
-      <Box flexBasis={300} flexShrink={0} className={classes.sideBarBox}>
-        <SideBar/>
-      </Box>
-      <Box flexGrow={1} className={classes.widgetBox}>
-        <WidgetsTemplate/>
-      </Box>
-    </Box>
-  )
+	const classes = useStyles();
+
+	return (
+		<Box display="flex" flexGrow={1} className={classes.mainBox}>
+			<Box display="flex" flexDirection="column" justifyContent="center" alignItems="stretch">
+				<Box flexBasis={300} flexShrink={0} className={classes.sideBarBox}>
+					<SideBar />
+				</Box>
+				<Box flexBasis={300} flexShrink={0} className={classes.sideBarBox}>
+					<LocationMap marker={{ lat: 32.0853, lng: 34.7818 }} />
+				</Box>
+			</Box>
+			<Box flexGrow={1} className={classes.widgetBox}>
+				<WidgetsTemplate />
+			</Box>
+		</Box>
+	);
 };
 
-export default HomePage
+export default HomePage;
