8bdff4166114fbfcfe4ad19062c06dd1715fc3e1
adele-angel <adeler.ang@gmail.com>
2020-03-18 21:06:35 +0200

    formatting with editorconfig

--- src/components/templates/SideBar.tsx
+++ src/components/templates/SideBar.tsx
@@ -6,22 +6,22 @@ import { NewsFlashFilterPanel } from "../atoms/NewsFlahsFilterPanel";
 interface IProps {}
 
 const useStyles = makeStyles({
-	root: {
-		borderLeft: "1px solid rgba(0, 0, 0, 0.12)"
-	}
+  root: {
+    borderLeft: "1px solid rgba(0, 0, 0, 0.12)"
+  }
 });
 
 export const SideBar: FunctionComponent<IProps> = () => {
-	const classes = useStyles();
+  const classes = useStyles();
 
-	return (
-		<Box className={classes.root}>
-			<NewsFlashFilterPanel />
-			<Box display="flex" flexDirection="column">
-				<Box flexGrow="1">
-					<News />
-				</Box>
-			</Box>
-		</Box>
-	);
+  return (
+    <Box className={classes.root}>
+      <NewsFlashFilterPanel />
+      <Box display="flex" flexDirection="column">
+        <Box flexGrow="1">
+          <News />
+        </Box>
+      </Box>
+    </Box>
+  );
 };
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -8,36 +8,41 @@ import LocationMap from "../components/molecules/LocationMap";
 interface IProps {}
 
 const useStyles = makeStyles({
-	mainBox: {
-		height: "inherit"
-	},
-	sideBarBox: {
-		height: "inherit",
-		overflow: "auto"
-	},
-	widgetBox: {
-		height: "inherit",
-		overflow: "auto"
-	}
+  mainBox: {
+    height: "inherit"
+  },
+  sideBarBox: {
+    height: "inherit",
+    overflow: "auto"
+  },
+  widgetBox: {
+    height: "inherit",
+    overflow: "auto"
+  }
 });
 const HomePage: FunctionComponent<IProps> = () => {
-	const classes = useStyles();
+  const classes = useStyles();
 
-	return (
-		<Box display="flex" flexGrow={1} className={classes.mainBox}>
-			<Box display="flex" flexDirection="column" justifyContent="center" alignItems="stretch">
-				<Box flexBasis={300} flexShrink={0} className={classes.sideBarBox}>
-					<SideBar />
-				</Box>
-				<Box flexBasis={300} flexShrink={0} className={classes.sideBarBox}>
-					<LocationMap marker={{ lat: 32.0853, lng: 34.7818 }} />
-				</Box>
-			</Box>
-			<Box flexGrow={1} className={classes.widgetBox}>
-				<WidgetsTemplate />
-			</Box>
-		</Box>
-	);
+  return (
+    <Box display="flex" flexGrow={1} className={classes.mainBox}>
+      <Box
+        display="flex"
+        flexDirection="column"
+        justifyContent="center"
+        alignItems="stretch"
+      >
+        <Box flexBasis={300} flexShrink={0} className={classes.sideBarBox}>
+          <SideBar />
+        </Box>
+        <Box flexBasis={300} flexShrink={0} className={classes.sideBarBox}>
+          <LocationMap marker={{ lat: 32.0853, lng: 34.7818 }} />
+        </Box>
+      </Box>
+      <Box flexGrow={1} className={classes.widgetBox}>
+        <WidgetsTemplate />
+      </Box>
+    </Box>
+  );
 };
 
 export default HomePage;
