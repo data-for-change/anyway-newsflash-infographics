076d861c0b48d2a948cc5517fa66dff00fdaec44
sejoB <sejo1981@gmail.com>
2020-04-14 15:12:15 +0300

    added reverse method for hebrew data

--- src/components/molecules/CountByYearBarWidget.tsx
+++ src/components/molecules/CountByYearBarWidget.tsx
@@ -1,10 +1,11 @@
 import React, { FunctionComponent } from 'react'
 import BarChartView from './BarChartView';
 import { IWidgetAccidentsByYear } from '../../models/WidgetData'
+import { rightToLeftText } from '../../utils/utils';
 
 const ACCIDENT_YEAR = 'accident_year'
 const COUNT = 'count'
-const TEXT = 'הנשב תונואת תומכ';
+const TEXT = rightToLeftText('כמות תאונות בשנה');
 
 interface IProps {
 	data: IWidgetAccidentsByYear[]
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -19,3 +19,14 @@ export function uniquePoints(points: IPoint[]) {
 export function getAPIKey(){
   return process.env.NODE_ENV ==='production'? process.env.REACT_APP_GOOGLE_MAPS_API_KEY:'';
 }
+//temporary solution
+const isHebrew = (text: string) => {
+  return text.search(/[\u0590-\u05FF]/) >= 0;
+}
+export function rightToLeftText(text: string) {
+  if (isHebrew(text)) {
+    return text.split('').reverse().join('');
+  } else {
+    return text;
+  }
+}
