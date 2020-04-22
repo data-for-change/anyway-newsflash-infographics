982b15e32902ade1b83d3992cd0b40b4440e3545
sejoB <sejo1981@gmail.com>
2020-04-22 16:34:38 +0300

    have a good quality, but need more work on layers

--- src/components/molecules/BarChartView.tsx
+++ src/components/molecules/BarChartView.tsx
@@ -1,5 +1,6 @@
 import React, { FunctionComponent } from 'react'
 import { ResponsiveContainer, BarChart, LabelList, XAxis, Label, Bar } from 'recharts'
+import { position } from 'html2canvas/dist/types/css/property-descriptors/position'
 
 interface IProps {
 	data: Array<object>
@@ -7,18 +8,28 @@ interface IProps {
   yLabel: string | number
   textLabel: string
 }
+const renderCustomizedLabel = (props:any) => {
+  const { x, y, width, height, value } = props;
 
+  return (
+    <g transform={`translate(${x + width / 2},${(y+height)-2})`}>
+      <text x={0} y={0} fill="black" textAnchor="end" transform="rotate(-90)" dominantBaseline="middle" >
+        {value}
+      </text>
+    </g>
+  );
+};
 const BarChartView: FunctionComponent<IProps> = ({data, xLabel, yLabel, textLabel}) => {
 
 	return (
     <ResponsiveContainer width={'100%'} height={'90%'}>
       <BarChart data={data} margin={{ top: 20, bottom: 15 }}>
-        <XAxis dataKey='none' tickLine={false} axisLine={false}>
-          <Label value={textLabel} offset={1} position='bottom' />
+        <XAxis dataKey="none" tickLine={false} axisLine={false}>
+          <Label value={textLabel} offset={1} position="bottom" />
         </XAxis>
         <Bar dataKey={yLabel} fill='#c43a31'>
-          <LabelList dataKey={xLabel} angle={90} position='insideBottomLeft' />
-          <LabelList dataKey={yLabel} position='top' />
+          <LabelList dataKey={xLabel} content={renderCustomizedLabel} />
+          <LabelList dataKey={yLabel} position="top" />
         </Bar>
       </BarChart>
     </ResponsiveContainer>
