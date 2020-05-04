0b68a800db92a401d0649fc06894972f2183017a
9syncopation <9syncopation@gmail.com>
2020-05-04 12:23:03 +0300

    image styling

--- src/components/molecules/StaticImageWidget.tsx
+++ src/components/molecules/StaticImageWidget.tsx
@@ -1,16 +1,23 @@
 import React, { FC } from 'react';
 import { IWidgetStaticImg } from '../../models/WidgetData';
-import Vision_Zero from '../../assets/vision_zero.png';
+import VisionZeroImg from '../../assets/vision_zero.png';
+import { SignalWifi1BarLockSharp } from '@material-ui/icons';
+import { green } from '@material-ui/core/colors';
 
-
+let imageStyles = {
+    margin: '15px',
+	backgroundColor: 'gray',
+	border: '5px dashed green',
+	width: '90%',
+	height: '80%',
+	
+  };
 interface IProps {
   data: IWidgetStaticImg[];
 }
-const StaticImg: FC<IProps> = ({data}) => {
-	console.log( data)
-
-	return (<div >'hello'
-		<img src= {Vision_Zero} alt="vision zero" />
-	</div>);
+const StaticImg: FC<IProps> = () => {
+	return (
+		<img src= {VisionZeroImg} style= {imageStyles} alt="vision zero" />
+	);
 };
 export default StaticImg;
