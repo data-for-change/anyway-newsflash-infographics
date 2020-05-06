83b63b00465fdac5923ca65076e62be31ee4af53
sejoB <sejo1981@gmail.com>
2020-05-06 18:01:56 +0300

    move link to env file

--- src/components/molecules/LogInLinkGoogle.tsx
+++ src/components/molecules/LogInLinkGoogle.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { makeStyles } from '@material-ui/core/styles';
 import { onLinkColor, onLinkColorHover } from '../../style/_globals';
 
-const GOOGLE_LOGIN_LINK = 'https://anyway-mock-server.herokuapp.com/auth/google-login';
+const LINK = process.env.REACT_APP_GOOGLE_LOGIN_LINK;
 
 const useStyles = makeStyles({
   link: {
@@ -11,12 +11,13 @@ const useStyles = makeStyles({
     '&:hover': {
       color: `${onLinkColorHover}`,
     },
+    cursor: 'pointer'
   },
 });
 const LogInLinkGoogle = () => {
   const classes = useStyles();
 
-    return <a className={classes.link}  href={GOOGLE_LOGIN_LINK}>LOGIN</a>
+    return <a className={classes.link}  href={LINK} >LOGIN</a>
 };
 
 export default LogInLinkGoogle
