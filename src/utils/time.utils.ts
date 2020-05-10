0ead78b11bed44b639fbe3d286da3710c706567a
sejoB <sejo1981@gmail.com>
2020-05-10 16:05:10 +0300

    #221 bug solved

--- src/utils/time.utils.ts
+++ src/utils/time.utils.ts
@@ -1,12 +1,13 @@
-export function dateFormat(date: Date| string): string {
-  let dateStr = '';
-  if(!date) {
-    console.error('invalid date', date);
+
+export function dateFormat ( date: Date | string ): string {
+  let dateStr = ''
+  if ( !date ) {
+    console.error( 'invalid date', date )
   } else {
-    if(typeof date === 'string') {
-      date = new Date(date);
+    if ( typeof date === 'string' ) {
+      date = new Date( date )
     }
-    dateStr = `${date.getDay()}.${date.getMonth()}.${date.getFullYear()}`
+    dateStr = new Intl.DateTimeFormat( 'he-il' ).format( date )
   }
-  return dateStr;
+  return dateStr
 }
