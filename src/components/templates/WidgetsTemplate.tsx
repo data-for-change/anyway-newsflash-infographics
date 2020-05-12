ea12661bafca9326f77df5f3e308b1c3a772b65f
sejoB <sejo1981@gmail.com>
2020-05-12 16:19:14 +0300

    added table, connected to data, need work on style

--- src/components/templates/WidgetsTemplate.tsx
+++ src/components/templates/WidgetsTemplate.tsx
@@ -12,6 +12,7 @@ import CountAccidentsByDayNightPieWidget from '../molecules/CountAccidentsByDayN
 import StreetViewWidget from '../molecules/StreetViewWidget';
 import VisionZeroImageViewWidget from '../molecules/VisionZeroImageViewWidget';
 import MostSevereAccidentsMapWidget from '../molecules/MostSevereAccidentsMapWidget';
+import MostSevereAccidentsTableWidget from '../molecules/MostSevereAccidentsTableWidget';
 import HeatMap from '../molecules/HeatMap';
 import ErrorBoundary from '../atoms/ErrorBoundary';
 import { MetaTag } from '../atoms';
@@ -25,44 +26,49 @@ const getWidgetByType = (widget: any) => {
   const { name, data } = widget;
   let widgetComponent;
   switch (name) {
-    case 'most_severe_accidents': {
-      widgetComponent = <MostSevereAccidentsMapWidget data={data} />;
+    // case 'most_severe_accidents': {
+    //   widgetComponent = <MostSevereAccidentsMapWidget data={data} />;
+    //   break;
+    // }
+    case 'most_severe_accidents_table': {
+      // console.log(name ,data);
+      widgetComponent = <MostSevereAccidentsTableWidget data={data} />;
       break;
     }
-    case 'accidents_heat_map': {
-      widgetComponent = <HeatMap data={data.items} center={{ lat: 32.0853, lng: 34.7818 }} />;
-      break;
-    }
-    case 'street_view': {
-      widgetComponent = <StreetViewWidget data={data} />;
-      break;
-    }
-    case 'accident_count_by_severity': {
-      //widget wait for data changes from server
-      widgetComponent = <CountBySeverityTextWidget data={data} />;
-      break;
-    }
-    case 'accident_count_by_accident_type': {
-      // example of pie widget
-      widgetComponent = <CountByTypePieWidget data={data} />;
-      break;
-    }
-    case 'accident_count_by_accident_year': {
-      widgetComponent = <CountByYearBarWidget data={data} />;
-      break;
-    }
-    case 'injured_count_by_accident_year': {
-      widgetComponent = <CountInjuredByYearBarWidget data={data} />;
-      break;
-    }
-    case 'accident_count_by_day_night': {
-      widgetComponent = <CountAccidentsByDayNightPieWidget data={data} />;
-      break;
-	}
-	case 'vision_zero_view': {
-		widgetComponent = <VisionZeroImageViewWidget data={data} />;
-		break;
-	}
+    // case 'accidents_heat_map': {
+    //   widgetComponent = <HeatMap data={data.items} center={{ lat: 32.0853, lng: 34.7818 }} />;
+    //   break;
+    // }
+    // case 'street_view': {
+    //   widgetComponent = <StreetViewWidget data={data} />;
+    //   break;
+    // }
+    // case 'accident_count_by_severity': {
+    //   //widget wait for data changes from server
+    //   widgetComponent = <CountBySeverityTextWidget data={data} />;
+    //   break;
+    // }
+    // case 'accident_count_by_accident_type': {
+    //   // example of pie widget
+    //   widgetComponent = <CountByTypePieWidget data={data} />;
+    //   break;
+    // }
+    // case 'accident_count_by_accident_year': {
+    //   widgetComponent = <CountByYearBarWidget data={data} />;
+    //   break;
+    // }
+    // case 'injured_count_by_accident_year': {
+    //   widgetComponent = <CountInjuredByYearBarWidget data={data} />;
+    //   break;
+    // }
+    // case 'accident_count_by_day_night': {
+    //   widgetComponent = <CountAccidentsByDayNightPieWidget data={data} />;
+    //   break;
+    // }
+    // case 'vision_zero_view': {
+    //   widgetComponent = <VisionZeroImageViewWidget data={data} />;
+    //   break;
+    // }
     default: {
       widgetComponent = null; // do not create element for unrecognized widget
       console.warn(`widget name (${name}) was not recognize `, widget);
@@ -96,8 +102,8 @@ const WidgetsTemplate: FC<IProps> = ({ id }) => {
   });
 
  const NoDataText = <Text type={TextType.CONTENT_TITLE}>אין נתונים להצגה</Text>
- 
- return  <Grid.Container>{widgetsData && widgetsData.length > 0 ? widgetCards: NoDataText} </Grid.Container> 
+
+ return  <Grid.Container>{widgetsData && widgetsData.length > 0 ? widgetCards: NoDataText} </Grid.Container>
 };
 
 export default observer(WidgetsTemplate);
