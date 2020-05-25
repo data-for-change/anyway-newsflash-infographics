6cc9dfef72851315b3ba3dd8401c50e494e6d4ba
yuvalbl <yuval.bar.levi@gmail.com>
2020-05-25 17:23:30 +0300

    add head on collision basic styling

--- src/components/molecules/widgets/HeadOnCollisionsComparisonWidget.tsx
+++ src/components/molecules/widgets/HeadOnCollisionsComparisonWidget.tsx
@@ -3,7 +3,7 @@ import PieChartView from '../PieChartView';
 import { IWidgetHeadOnCollisionsComparisonData } from '../../../models/WidgetData';
 import { Box, makeStyles, Theme } from '@material-ui/core';
 import roadNumberIcon from '../../../assets/road90.svg.png';
-import { cardContentHeight } from '../../../style';
+import { cardContentHeight, highlightBasicColor } from '../../../style';
 
 const ACCIDENT_TYPE = 'desc';
 const COUNT = 'count';
@@ -15,9 +15,14 @@ interface IProps {
 }
 
 const useStyles = makeStyles((theme: Theme) => ({
-  large: {
+  roadNumber: {
     width: theme.spacing(7),
     height: theme.spacing(5),
+    padding: theme.spacing(2),
+  },
+  textHighlight: {
+    backgroundColor: highlightBasicColor,
+    textAlign: 'center',
   }
 }));
 
@@ -28,15 +33,19 @@ const HeadOnCollisionsComparisonWidget: FC<IProps> = ({ data }) => {
   return (
     <Box display="flex" flexDirection="column">
       <Box display="flex" height={MAIN_CONTENT_HEIGHT} width={'100%'}>
-        <Box flexBasis={100}>
-          <img alt="Road Number" src={roadNumberIcon} className={classes.large} />
-          <span>בכביש 90 מקטע מצפה שלם - צומת שדי תרומות</span>
+        <Box flexBasis={120} display="flex" flexDirection="column" justifyContent="center" alignItems="center">
+          <img alt="Road Number" src={roadNumberIcon} className={classes.roadNumber} />
+          <span className={classes.textHighlight}>בכביש 90 מקטע מצפה שלם - צומת שדי תרומות</span>
         </Box>
         <PieChartView data={bigPieData} xLabel={ACCIDENT_TYPE} yLabel={COUNT} />
       </Box>
       <Box display="flex" height={SECONDARY_CONTENT_HEIGHT} width={'100%'}>
         <PieChartView data={smallPieData} xLabel={ACCIDENT_TYPE} yLabel={COUNT} />
-        <Box flexBasis={250}>בכבישים עירוניים (ללא צמתים) בכל הארץ</Box>
+        <Box flexBasis={280} display="flex" alignItems="center">
+          <span className={classes.textHighlight}>
+          בכבישים עירוניים (ללא צמתים) בכל הארץ
+          </span>
+          </Box>
       </Box>
     </Box>
   );
