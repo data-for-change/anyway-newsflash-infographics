8bc2379083c4fc85da4e24a060e289c9c44585d3
Yogev Boaron Ben-Har <yogiboaron@gmail.com>
2020-05-27 22:23:56 +0300

    feat: Add a link from Header to homepage
    Wrap Logo with `AnywayLink` component

--- src/components/molecules/Header.tsx
+++ src/components/molecules/Header.tsx
@@ -1,47 +1,51 @@
-import React, {FC, useEffect} from 'react';
+import React, { FC, useEffect } from 'react';
 import { observer } from 'mobx-react-lite';
 import { AnywayAppBar } from '../atoms/AnywayAppBar';
 import { Logo } from '../atoms/Logo';
 import AnywayImage from '../../assets/anyway.png';
 import { SignInIcon } from '../atoms/SignInIcon';
 import LogInLinkGoogle from './LogInLinkGoogle';
-import { Box } from '@material-ui/core'
-import {useStore} from '../../store/storeConfig';
+import { Box } from '@material-ui/core';
+import { useStore } from '../../store/storeConfig';
 import RootStore from '../../store/root.store';
 import UserProfileHeader from './UserProfileHeader';
-import {makeStyles} from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
+import { AnywayLink } from '../atoms';
 
 const useStyles = makeStyles({
-  profileBox:{
-  display:'flex'
-}
+  profileBox: {
+    display: 'flex',
+  },
 });
 
- const Header: FC = () => {
-  const store : RootStore = useStore();
+const Header: FC = () => {
+  const store: RootStore = useStore();
   const classes = useStyles();
-  useEffect(()=>{
+  useEffect(() => {
     store.getUserLoginDetails();
-  },[store]);
+  }, [store]);
   //login or logout- depend on authentication state
   let authElement;
-  if(store.isUserAuthenticated){
-    authElement = <UserProfileHeader name={store.userName}/>
-  }
-  else{
-    authElement = <div>
-      <LogInLinkGoogle/>
-    </div>
+  if (store.isUserAuthenticated) {
+    authElement = <UserProfileHeader name={store.userName} />;
+  } else {
+    authElement = (
+      <div>
+        <LogInLinkGoogle />
+      </div>
+    );
   }
   return (
     <AnywayAppBar>
-      <Logo src={AnywayImage} alt={'Anyway'} height={'30px'} />
-      <Box className={classes.profileBox} >
+      <AnywayLink to="/">
+        <Logo src={AnywayImage} alt={'Anyway'} height={'30px'} />
+      </AnywayLink>
+      <Box className={classes.profileBox}>
         {authElement}
-        <SignInIcon/>
+        <SignInIcon />
       </Box>
     </AnywayAppBar>
   );
 };
 
- export default observer(Header);
+export default observer(Header);
