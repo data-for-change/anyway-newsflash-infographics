129a7c25b6ea6f22e1fa9ca4d57131c2423da0ff
sejoB <sejo1981@gmail.com>
2020-05-27 21:37:55 +0300

    solved #269  created sidebarMap component

--- src/components/atoms/MapIcon.tsx
+++ src/components/atoms/MapIcon.tsx
@@ -1,12 +1,21 @@
 import accidentIcon from '../../assets/accident_icon.png';
 import AnyWayMapIcon from './AnywayMapIcon';
+import redIcon from '../../assets/marker-icon-2x-red.png';
+import orangeIcon from '../../assets/marker-icon-2x-orange.png';
+import yellowIcon from '../../assets/marker-icon-2x-yellow.png';
+import shadowIcon from '../../assets/marker-shadow.png';
 
-const AccidentIcon = () => {
-    return AnyWayMapIcon( accidentIcon );
-};
+
+const accidentMarker = () => AnyWayMapIcon( accidentIcon );
+const redMarker = () => AnyWayMapIcon(redIcon, shadowIcon);
+const orangeMarker = () => AnyWayMapIcon(orangeIcon, shadowIcon);
+const yellowMarker = () => AnyWayMapIcon( yellowIcon, shadowIcon );
 
 const MapIcon = {
-    AccidentIcon
+    accidentMarker,
+    redMarker,
+    orangeMarker,
+    yellowMarker
 };
 
 export default MapIcon;
