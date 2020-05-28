2a45476579443dc8634f3e5019c6ca43461ea7da
sejoB <sejo1981@gmail.com>
2020-05-28 12:53:23 +0300

    solved #269

--- src/components/atoms/AnywayMostSevereAccidentsMarker.tsx
+++ src/components/atoms/AnywayMostSevereAccidentsMarker.tsx
@@ -1,7 +1,7 @@
 import React, { FC } from 'react';
 import ReactDOMServer from 'react-dom/server';
 import L from 'leaflet';
-import { Marker } from 'react-leaflet';
+import { Marker, Popup } from 'react-leaflet';
 import { makeStyles } from '@material-ui/core';
 import { dateFormat } from '../../utils/time.utils';
 import MapIcon from '../atoms/MapIcon'
@@ -73,7 +73,15 @@ const AnywayMostSevereAccidentsMarker: FC<IProps> = ( { markerdata, markerside }
   return !accident_timestamp && !accident_severity ? null : (
     <>
       <Marker icon={icon} position={lPoint} />
-      <Marker icon={MapIcon.accidentMarker()} position={lPoint} />
+      <Marker icon={MapIcon.accidentMarker()} position={lPoint}>
+        {
+          <Popup>
+            <div>
+              {markerdata.accident_timestamp}, {markerdata.accident_severity}
+            </div>
+          </Popup>
+        }
+      </Marker>
     </>
   );
 };
