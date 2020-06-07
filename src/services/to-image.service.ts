c0041dd29a21a704f9dc192a2e2bc9a94131da50
sejoB <sejo1981@gmail.com>
2020-06-07 16:44:46 +0300

    #303 solved

--- src/services/to-image.service.ts
+++ src/services/to-image.service.ts
@@ -3,24 +3,28 @@ import html2canvas from 'html2canvas';
 import domtoimage from 'dom-to-image';
 import { cardHeight } from '../style';
 
+const removeMapControllers = (el: HTMLDocument) => {
+  const elementList = el.querySelectorAll(
+    '.leaflet-control-container, .gmnoprint, .gm-fullscreen-control, .gm-iv-address, .gm-style-cc',
+  );
+  Array.from(elementList).forEach((el) => el.remove());
+};
 const widgetToImage = (fileName: string, widgetElement: HTMLElement) => {
   const containMap = widgetElement.querySelector('canvas, .leaflet-container');
   console.log(`Download image using ${containMap ? 'Html2Canvas' : 'DomToImage'}`);
-  
-  if(containMap) {
+  if (containMap) {
     usingHtml2Canvas(fileName, widgetElement);
   } else {
     usingDomToImage(fileName, widgetElement);
   }
 };
-
 // Uses canvas. OK for maps, buggy for other elements
 // https://github.com/niklasvh/html2canvas
 const usingHtml2Canvas = (fileName: string, widgetElement: HTMLElement) => {
   html2canvas(widgetElement, {
     useCORS: true, // to allow loading maps
     imageTimeout: 3000,
-    ignoreElements: (AnyWayButtton) => false,
+    onclone: (el) => removeMapControllers(el),
   })
     .then(function (canvas) {
       console.log(canvas);
@@ -30,15 +34,15 @@ const usingHtml2Canvas = (fileName: string, widgetElement: HTMLElement) => {
       saveAs(blob, `${fileName}.png`);
     });
 };
-
 // Uses canvas. OK for Html/SVG, buggy for maps
 // https://github.com/tsayen/dom-to-image
 const usingDomToImage = (fileName: string, widgetElement: HTMLElement) => {
-  domtoimage
-    .toBlob(widgetElement, { height: cardHeight })
-    .then(function (blob: any) {
-      window.saveAs(blob, `${fileName}.png`);
-    });
+  const filter = (widgetElement: any) => {
+    return widgetElement.tagName !== 'BUTTON';
+  };
+  domtoimage.toBlob(widgetElement, { height: cardHeight, filter: filter }).then(function (blob: any) {
+    window.saveAs(blob, `${fileName}.png`);
+  });
 };
 
 export default widgetToImage;
