e48b92675259fcb1870b6006116cc4ed274ce082
sejoB <sejo1981@gmail.com>
2020-06-16 17:17:07 +0300

    change icon size for static sizes

--- src/services/get-road-image.service.tsx
+++ src/services/get-road-image.service.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import { Theme, makeStyles } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core';
 import road90 from '../assets/road90.svg.png';
 
-const useStyles = makeStyles((theme: Theme) => ({
+const useStyles = makeStyles({
   root: {
-    width: theme.spacing(7),
-    height: theme.spacing(5),
+    width: '56px',
+    height: '40px',
   },
-}));
+});
 const RoadNumberImage = () => {
   const classes = useStyles();
   return <img src={road90} className={classes.root} />;
