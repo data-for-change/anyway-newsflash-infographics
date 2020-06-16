7b1ac9c651d00c90c4c7e3bae77151cdc0f2bcdf
Yael Luria <yaeluria@gmail.com>
2020-06-17 01:17:40 +0300

    move button from inside dialog component to footer

--- src/components/templates/Footer.tsx
+++ src/components/templates/Footer.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React, { FC, useState} from 'react';
 import logoHasdna from '../../assets/hasadna.png';
 import { makeStyles } from '@material-ui/core/styles';
 import { Box } from '@material-ui/core';
@@ -31,15 +31,25 @@ const useStyles = makeStyles({
 });
 
 export const Footer: FC<IProps> = () => {
+  const [isShowingAbout, setIsShowingAbout] = useState(false);
+  const [isShowingThank, setIsShowingThank] = useState(false);
+  const toggleAbout = () => {
+    setIsShowingAbout(!isShowingAbout)
+  }
+  const toggleThank = () => {
+    setIsShowingThank(!isShowingThank)
+  }
   const classes = useStyles();
   return (
-    <footer className={classes.footer} >
+    <footer className={classes.footer}>
       <Box className={classes.links}>
-      <a href="https://www.hasadna.org.il/" target="_blank" rel="noopener noreferrer">
-        <img src={logoHasdna} alt="logo-hasadna" className={classes.logo} />
-      </a>
-       <AboutDialog />
-       <ThankYouDialog />
+        <a href="https://www.hasadna.org.il/" target="_blank" rel="noopener noreferrer">
+          <img src={logoHasdna} alt="logo-hasadna" className={classes.logo} />
+        </a>
+        <Box onClick={toggleAbout}>אודות</Box>
+        <AboutDialog isShowing={isShowingAbout} toggle={toggleAbout} />
+        <Box onClick={toggleThank}>תודות</Box>
+        <ThankYouDialog isShowing={isShowingThank} toggle={toggleThank} />
       </Box>
     </footer>
   );
