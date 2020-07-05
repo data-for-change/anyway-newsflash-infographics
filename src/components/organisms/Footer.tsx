fb2d0ff716d7223f7efa6f697864494470df215b
Yael Luria <yaeluria@gmail.com>
2020-07-05 12:41:49 +0300

    wrap text with Text atom, change styles for footer links solves #326

--- src/components/organisms/Footer.tsx
+++ src/components/organisms/Footer.tsx
@@ -2,6 +2,7 @@ import React, { FC, useState } from 'react';
 import logoHasdna from '../../assets/hasadna.png';
 import { makeStyles } from '@material-ui/core/styles';
 import { Box } from '@material-ui/core';
+import { Text, TextType } from '../atoms';
 import { borderColor, onLinkColor, onLinkColorHover } from '../../style';
 import ThankYouDialog from './ThankYouDialog';
 import AboutDialog from './AboutDialog';
@@ -10,6 +11,7 @@ interface IProps {}
 const useStyles = makeStyles({
   logo: {
     height: '30px',
+    marginRight: '0.5rem',
   },
   links: {
     display: 'flex',
@@ -19,13 +21,17 @@ const useStyles = makeStyles({
     '&:hover': {
       color: `${onLinkColorHover}`,
     },
-    justifyContent: 'space-evenly',
-    flexGrow: 1,
+    justifyContent: 'flex-start',
   },
   footer: {
     flexGrow: 1,
     display: 'flex',
     border: `1px solid ${borderColor}`,
+  },
+  linkItem: {
+    marginRight: '0.5rem',
+    paddingLeft: '0.5rem',
+    borderLeft: `2px solid ${borderColor}`,
   },
 });
 
@@ -45,9 +51,13 @@ export const Footer: FC<IProps> = () => {
         <a href="https://www.hasadna.org.il/" target="_blank" rel="noopener noreferrer">
           <img src={logoHasdna} alt="logo-hasadna" className={classes.logo} />
         </a>
-        <Box onClick={toggleAbout}>אודות</Box>
+        <Box className={classes.linkItem} onClick={toggleAbout}>
+          <Text type={TextType.CONTENT_TITLE}>אודות</Text>
+        </Box>
         <AboutDialog isShowing={isShowingAbout} onClose={toggleAbout} />
-        <Box onClick={toggleThank}>תודות</Box>
+        <Box className={classes.linkItem} onClick={toggleThank}>
+          <Text type={TextType.CONTENT_TITLE}>תודות</Text>
+        </Box>
         <ThankYouDialog isShowing={isShowingThank} onClose={toggleThank} />
       </Box>
     </footer>
