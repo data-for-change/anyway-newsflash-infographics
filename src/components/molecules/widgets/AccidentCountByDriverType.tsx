a26f90aca9217414eb7522dcef5a12c525ae8335
9syncopation <9syncopation@gmail.com>
2020-07-20 21:00:38 +0300

    implement widget accidnet coubt by driver type

--- src/components/molecules/widgets/AccidentCountByDriverType.tsx
+++ src/components/molecules/widgets/AccidentCountByDriverType.tsx
@@ -1,11 +1,14 @@
 import React, { FC } from 'react';
 import { IWidgetAccidentCountByDriverType } from '../../../models/WidgetData';
-
+import PieChartView from '../PieChartView';
 interface IProps {
   data: IWidgetAccidentCountByDriverType;
 }
+const DRIVER_TYPE = 'driver_type';
+const COUNT = 'count';
+const INNER_RADIUS = '30%';
 
-const AccidentCountByDriverType: FC<IProps> = () => {
-  return <div> {} </div>;
+const AccidentCountByDriverType: FC<IProps> = ({ data }) => {
+  return <PieChartView data={data.items} xLabel={DRIVER_TYPE} yLabel={COUNT} innerRadius={INNER_RADIUS} />;
 };
 export default AccidentCountByDriverType;
