f77027d4b5cc51196ff7e87922772bbc0fb0fb2b
9syncopation <9syncopation@gmail.com>
2020-07-28 00:13:35 +0300

    reafctor - place const variable afetr imports

--- src/components/molecules/widgets/AccidentCountByDriverType.tsx
+++ src/components/molecules/widgets/AccidentCountByDriverType.tsx
@@ -1,12 +1,13 @@
 import React, { FC } from 'react';
 import { IWidgetAccidentCountByDriverType } from '../../../models/WidgetData';
 import PieChartView from '../PieChartView';
-interface IProps {
-  data: IWidgetAccidentCountByDriverType;
-}
+
 const DRIVER_TYPE = 'driver_type';
 const COUNT = 'count';
 const INNER_RADIUS = '30%';
+interface IProps {
+  data: IWidgetAccidentCountByDriverType;
+}
 
 const AccidentCountByDriverType: FC<IProps> = ({ data }) => {
   return <PieChartView data={data.items} xLabel={DRIVER_TYPE} yLabel={COUNT} innerRadius={INNER_RADIUS} />;
