4f918a0ab78cc2ce4354f33ba11614fcd879279c
Yael Luria <yaeluria@gmail.com>
2020-07-29 01:24:25 +0300

    use menu atom instead of links for language buttons

--- src/components/molecules/LanguageButtons.tsx
+++ src/components/molecules/LanguageButtons.tsx
@@ -1,9 +1,10 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import { useStore } from '../../store/storeConfig';
 import RootStore from '../../store/root.store';
 import { makeStyles } from '@material-ui/core/styles';
 import { onLinkColor, onLinkColorHover } from '../../style';
-import Text, { TextType } from '../atoms/Text';
+import Menu from '../atoms/Menu';
+import languageSelector from '../../assets/language-selector.svg';
 import { AnyWayButton } from '../atoms/AnyWayButton';
 
 const useStyles = makeStyles({
@@ -34,21 +35,25 @@ const LANGUAGES = [
 const LanguageButtons: FC = () => {
   const store: RootStore = useStore();
   const classes = useStyles();
+  const [anchorEl, setAnchorEl] = useState(null);
+  const openMenu = (event: any) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const closeMenu = () => {
+    setAnchorEl(null);
+  };
+  const handleLanguageChoice = (value: 'he' | 'en' | 'ar') => {
+    //todo? - change url on click to reflect language
+    store.changeLanguage(value);
+  };
+
   return (
     <div>
-      {languages.map((language) => {
-        return (
-          <AnyWayButton
-            //todo - button should be disabled if it represents the current language
-            className={classes.link}
-            onClick={() => {
-              store.changeLanguage(language.value);
-            }}
-          >
-            <Text type={TextType.CONTENT_TITLE}>{language.buttonText}</Text>
-          </AnyWayButton>
-        );
-      })}
+      {/* todo - add chevron? */}
+      <AnyWayButton aria-controls="menu" aria-haspopup="true" onClick={openMenu} className={classes.link}>
+        <img src={languageSelector} />
+      </AnyWayButton>
+      <Menu data={LANGUAGES} handleChoice={handleLanguageChoice} handleClose={closeMenu} anchorEl={anchorEl} />
     </div>
   );
 };
