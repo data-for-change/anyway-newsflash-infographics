28dd5dfa38f8d4c7bc3e312343da470e5a057867
Yael Luria <yaeluria@gmail.com>
2020-07-29 21:23:42 +0300

    replace data array with array of links and remove onclick function to implement routing instead

--- src/components/atoms/Menu.tsx
+++ src/components/atoms/Menu.tsx
@@ -1,22 +1,20 @@
-import React, { FC } from 'react';
+import React, { FC, ReactElement } from 'react';
 import MaterialMenu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
-import { Text, TextType } from '../atoms';
 
 interface IProps {
-  data: Array<any>;
+  items: Array<ReactElement>;
   handleClose: () => any;
   anchorEl: HTMLElement | null;
-  handleChoice: (value: any) => any;
 }
-const Menu: FC<IProps> = ({ data, anchorEl, handleClose, handleChoice }) => {
+const Menu: FC<IProps> = ({ items, anchorEl, handleClose }) => {
   return (
     <div>
       <MaterialMenu id="menu" anchorEl={anchorEl} keepMounted open={Boolean(anchorEl)} onClose={handleClose}>
-        {data.map((option, index) => {
+        {items.map((item, index) => {
           return (
-            <MenuItem key={index} onClick={() => handleChoice(option.value)}>
-              <Text type={TextType.CONTENT_TITLE}>{option.buttonText}</Text>
+            <MenuItem key={index}>
+              {item}
             </MenuItem>
           );
         })}
--- src/components/molecules/LanguageMenu.tsx
+++ src/components/molecules/LanguageMenu.tsx
@@ -1,11 +1,11 @@
 import React, { FC, useState } from 'react';
-import { useStore } from '../../store/storeConfig';
-import RootStore from '../../store/root.store';
 import { makeStyles } from '@material-ui/core/styles';
 import { onLinkColor, onLinkColorHover } from '../../style';
 import Menu from '../atoms/Menu';
 import languageSelector from '../../assets/language-selector.svg';
 import { AnyWayButton } from '../atoms/AnyWayButton';
+import { AnywayLink } from '../atoms';
+import { Text, TextType } from '../atoms';
 
 const useStyles = makeStyles({
   link: {
@@ -33,7 +33,6 @@ const LANGUAGES = [
   },
 ];
 const LanguageMenu: FC = () => {
-  const store: RootStore = useStore();
   const classes = useStyles();
   const [anchorEl, setAnchorEl] = useState(null);
   const openMenu = (event: any) => {
@@ -42,17 +41,24 @@ const LanguageMenu: FC = () => {
   const closeMenu = () => {
     setAnchorEl(null);
   };
-  const handleLanguageChoice = (value: 'he' | 'en' | 'ar') => {
-    //todo? - change url on click to reflect language
-    store.changeLanguage(value);
-  };
 
   return (
     <div>
       <AnyWayButton aria-controls="menu" aria-haspopup="true" onClick={openMenu} className={classes.link}>
         <img src={languageSelector} />
       </AnyWayButton>
-      <Menu data={LANGUAGES} handleChoice={handleLanguageChoice} handleClose={closeMenu} anchorEl={anchorEl} />
+      <Menu
+        items={LANGUAGES.map(language => (
+          <AnywayLink
+          // todo - link to the current url with appropiate language code in it
+           to={language.value}
+           >
+            <Text type={TextType.CONTENT_TITLE}>{language.buttonText}</Text>
+          </AnywayLink>
+        ))}
+        handleClose={closeMenu}
+        anchorEl={anchorEl}
+      />
     </div>
   );
 };
