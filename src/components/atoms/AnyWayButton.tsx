0b3ba7cf285a041b48a63ac248beff5437165aeb
Yael Luria <yaeluria@gmail.com>
2020-07-29 21:37:26 +0300

    remove unused styling

--- src/components/atoms/AnyWayButton.tsx
+++ src/components/atoms/AnyWayButton.tsx
@@ -3,6 +3,7 @@ import { Button, ButtonProps } from '@material-ui/core';
 
 interface IProps extends ButtonProps {
   onClick: ( arg0: any ) => any;
+  // onClick : Function;
 }
 
 export const AnyWayButton: FC<IProps> = ({ ...props }) => <Button {...props} />;
