a61c417b2fb5ea4570d827d8881911b6accb6ca9
Daniel Shely <danielshely1@gmail.com>
2020-10-12 23:51:07 +0300

    refactor piechart position

--- src/components/molecules/widgets/HeadOnCollisionsComparisonWidget.tsx
+++ src/components/molecules/widgets/HeadOnCollisionsComparisonWidget.tsx
@@ -3,13 +3,12 @@ import { Text, TextType } from '../../atoms';
 import PieChartView, {renderCollisionCustomizedLabel} from '../PieChartView';
 import { IWidgetHeadOnCollisionsComparisonData } from '../../../models/WidgetData';
 import { Box, makeStyles, Theme } from '@material-ui/core';
-import { highlightBasicColor } from '../../../style';
 import { useTranslation } from 'react-i18next';
 
 const ACCIDENT_TYPE = 'desc';
 const COUNT = 'count';
 const MAIN_CONTENT_HEIGHT = 250;
-const SECONDARY_CONTENT_HEIGHT = 130;
+const SECONDARY_CONTENT_HEIGHT = 190;
 const PROPORTION: number = SECONDARY_CONTENT_HEIGHT / MAIN_CONTENT_HEIGHT;
 const ROAD_NUMBER_REGEX: string = 'כביש [0-9]+';
 const PRIMARY_FONT_SIZE = 14;
@@ -23,6 +22,21 @@ interface IProps {
 const useStyles = makeStyles((theme: Theme) => ({
   textHighlight: {
     color: '#8a1212',
+    textAlign:'end'
+
+  },
+  segmentDesc:{
+    color:'gray',
+    textAlign:'end'
+  },
+  secondaryContent:{
+    display: 'flex',
+    height :SECONDARY_CONTENT_HEIGHT,
+    width :'80%',
+    position: 'relative',
+    bottom: '30%',
+    left:'3%'
+
   },
 }));
 
@@ -34,31 +48,30 @@ const HeadOnCollisionsComparisonWidget: FC<IProps> = ({ data, segmetText, usePer
   const roadNumberSegment: string[] | null = segmetText.match(ROAD_NUMBER_REGEX);
   const descSegment: string = roadNumberSegment == null ? '' : segmetText.substr(roadNumberSegment[0].length);
   return (
-    <Box display="flex" flexDirection="column">
+    <Box height={'100%'} display="flex" flexDirection="column">
       <Box display="flex" height={MAIN_CONTENT_HEIGHT} width={'100%'}>
-        <Box flexBasis={120} display="flex" flexDirection="column" justifyContent="center" alignItems="center">
-          <Box display="flex" flexDirection="column">
+          <Box  display="flex" flexDirection="column">
             <span className={classes.textHighlight}>{roadNumberSegment == null ? null : roadNumberSegment[0]}</span>
-            <span>{descSegment}</span>
+            <span className={classes.segmentDesc} >{descSegment}</span>
           </Box>
-        </Box>
-        <PieChartView  data={bigPieData} xLabel={ACCIDENT_TYPE} yLabel={COUNT} usePercent={usePercent}
-                      />
+        <PieChartView customizedLabel={renderCollisionCustomizedLabel} data={bigPieData} xLabel={ACCIDENT_TYPE} yLabel={COUNT} usePercent={usePercent}
+        />
       </Box>
       <Box
-        display="flex"
-        height={SECONDARY_CONTENT_HEIGHT}
-        width={'100%'}
+       className={classes.secondaryContent}
         fontSize={(PROPORTION * PRIMARY_FONT_SIZE).toString() + 'px'}
       >
-        <PieChartView  data={smallPieData} xLabel={ACCIDENT_TYPE} yLabel={COUNT} usePercent={usePercent}
-                      />
-        <Box justifyContent={'center'} flexBasis={280} display="flex" flexDirection="column" alignItems="start">
-          <div className={classes.textHighlight}>
+        <Box justifyContent={'center'}  display="flex" flexDirection="column">
+          <div  className={classes.textHighlight}>
             <Text type={TextType.CONTENT}>{t('onUrban.road')}</Text>
           </div>
-          <Text type={TextType.CONTENT}>{t('onUrban.location')}</Text>
+          <div className={classes.segmentDesc}>
+          <Text  type={TextType.CONTENT}>{t('onUrban.location')}</Text>
+          </div>
         </Box>
+
+          <PieChartView customizedLabel={renderCollisionCustomizedLabel} data={smallPieData} xLabel={ACCIDENT_TYPE} yLabel={COUNT} usePercent={usePercent}
+          />
       </Box>
       <Text type={TextType.CONTENT}>{'* בין השנים 2015-2019'}</Text>
     </Box>
