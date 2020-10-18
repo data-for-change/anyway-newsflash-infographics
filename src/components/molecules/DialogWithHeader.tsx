fdcbc7e772d84c10a10b91ebc6019e7ba3f1a2bb
Meital Lazarovich <lazarovich.meital@gmail.com>
2020-10-18 13:37:03 +0300

    dialog padding fixed

--- src/components/molecules/DialogWithHeader.tsx
+++ src/components/molecules/DialogWithHeader.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from 'react';
-import { Dialog, AnywayAppBar, Text, TextType } from '../atoms';
-import { Box, makeStyles, IconButton } from '@material-ui/core';
+import { Dialog, Text, TextType } from '../atoms';
+import { Box, makeStyles, IconButton, DialogTitle } from '@material-ui/core';
 import CloseIcon from '@material-ui/icons/Close';
 import { closeButtonColor, primaryColor } from '../../style';
 
@@ -12,6 +12,12 @@ interface IProps {
   maxWidth?: 'xs' | 'sm' | 'md' | 'lg' | 'xl' | false;
 }
 const useStyles = makeStyles({
+  'dialog-header': {
+    padding: '0 12px 0 24px',
+    paddingBottom: 0,
+    boxShadow: '0px 2px 4px -1px rgba(0,0,0,0.2), 0px 4px 5px 0px rgba(0,0,0,0.14), 0px 1px 10px 0px rgba(0,0,0,0.12)',
+    backgroundColor: '#fafafa',
+  },
   bar: {
     display: 'flex',
     width: 'inherit',
@@ -27,14 +33,14 @@ const DialogWithHeader: FC<IProps> = ({ onClose, isShowing, title, fullWidth, ch
   const classes = useStyles();
   return (
     <Dialog isShowing={isShowing} onClose={onClose} fullWidth={fullWidth} maxWidth={maxWidth}>
-      <AnywayAppBar>
+      <DialogTitle className={classes['dialog-header']}>
         <Box className={classes.bar}>
           <Text type={TextType.CONTENT_TITLE}>{title}</Text>
           <IconButton onClick={onClose} className={classes.close}>
             <CloseIcon>close the dialog</CloseIcon>
           </IconButton>
         </Box>
-      </AnywayAppBar>
+      </DialogTitle>
       <Box display="flex" flexDirection="column" px={3} py={3}>
         {children}
       </Box>
