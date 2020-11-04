40ab6277afb3a4798344e953433d73f7eec6b5cd
Almog Langleben <almoglangleben@Almogs-MacBook-Pro-2.local>
2020-11-03 22:18:18 -0800

    added a comment

--- src/utils/time.utils.ts
+++ src/utils/time.utils.ts
@@ -4,6 +4,7 @@ export function dateFormat(date: Date | string): string {
     console.error('invalid date', date);
   } else {
     if (typeof date === 'string') {
+      // using replace to support safari format: "2017-07-12 19:30:00" ===> "2017-07-12T19:30:00"
       date = new Date(date.replace(/\s/, 'T'));
     }
     dateStr = new Intl.DateTimeFormat('he-il').format(date);
