cf9739cb5cf4ce698e453a204eaa5d35aeac62cc
Daniel Shely <danielshely1@gmail.com>
2020-11-05 16:27:20 +0200

    fix inifinte scrolling timing and reformat with  prittier on component

--- src/components/atoms/InfinitScroll.tsx
+++ src/components/atoms/InfinitScroll.tsx
@@ -1,34 +1,35 @@
 import React, { FC, useEffect, useCallback, useRef } from 'react';
 import { makeStyles } from '@material-ui/core';
+import { useStore } from '../../store/storeConfig';
 
 function isScrollEnd(element: HTMLDivElement) {
-  return (element.scrollTop + element.clientHeight) === element.scrollHeight;
+  return element.scrollTop + element.clientHeight > element.scrollHeight - 30;
 }
 
 const useStyles = makeStyles({
   root: {
     overflow: 'auto',
     position: 'relative',
-  }
+  },
 });
 
 interface IProps {
   onScrollEnd: () => any;
 }
 
-const InfinitScroll: FC<IProps> = ({ children, onScrollEnd}) => {
+const InfinitScroll: FC<IProps> = ({ children, onScrollEnd }) => {
   const classes = useStyles();
   const scrollList = useRef<HTMLDivElement>(null);
-
+  const store = useStore();
 
   const handleScroll = useCallback(() => {
-    if (scrollList.current !== null) {
+    if (scrollList.current !== null && !store.newsFlashLoading) {
       if (isScrollEnd(scrollList.current)) {
         onScrollEnd();
       }
     }
   }, [scrollList, onScrollEnd]);
-  
+
   useEffect(() => {
     if (scrollList.current) {
       scrollList.current.addEventListener('scroll', handleScroll);
@@ -39,7 +40,7 @@ const InfinitScroll: FC<IProps> = ({ children, onScrollEnd}) => {
     <div ref={scrollList} className={classes.root}>
       {children}
     </div>
-  )
+  );
 };
 
 export default InfinitScroll;
