1a023fe97d3a22e4ddccc73f920be389238e656f
Almog Langleben <almoglangleben@Almogs-MacBook-Pro-2.local>
2020-12-22 21:46:13 +0200

    update sb, added HeadOnCollisionsComparisonWidget story

--- src/components/atoms/Button.stories.tsx
+++ src/components/atoms/Button.stories.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
-import { action } from '@storybook/addon-actions';
-import Button from './Button';
+export {};
+// import React from 'react';
+// import { action } from '@storybook/addon-actions';
+// import Button from './Button';
 
-export default {
-  title: 'Buttons',
-};
+// export default {
+//   title: 'Buttons',
+// };
 
-// export const ButtonPrimary = () => <Button.standard onClick={action('clicked')}>Button Primary</Button>;
+// // export const ButtonPrimary = () => <Button.standard onClick={action('clicked')}>Button Primary</Button>;
