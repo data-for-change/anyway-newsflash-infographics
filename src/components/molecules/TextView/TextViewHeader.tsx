35374eae8753a4275ef569a086a2cd8ca137fd15
Daniel Shely <danielshely1@gmail.com>
2021-01-11 23:48:26 +0200

    added regexp to cut road prefix

--- src/components/molecules/TextView/TextViewHeader.tsx
+++ src/components/molecules/TextView/TextViewHeader.tsx
@@ -16,6 +16,7 @@ interface AProps {
 }
 
 const red = roadIconColors.red;
+const roadRegexp = /כביש \d+/;
 
 const AccidentsOccurred: FC<AProps> = ({ accidentsCount, singleType }) => {
   const { t, i18n } = useTranslation();
@@ -49,6 +50,7 @@ const AccidentsOccurred: FC<AProps> = ({ accidentsCount, singleType }) => {
 
 const TextViewHeader: React.FC<IProps> = ({ data: { items }, segmentText, singleType }) => {
   const { t } = useTranslation();
+  const segmentPresentationVal : string= segmentText.replace(roadRegexp,'');
   return (
     <>
       {items.end_year === items.start_year ? (
@@ -65,7 +67,7 @@ const TextViewHeader: React.FC<IProps> = ({ data: { items }, segmentText, single
         </>
       )}
       <Box mr={1}>
-        <Typography.Body1>{t('textView.on') + segmentText}</Typography.Body1>
+        <Typography.Body1>{segmentPresentationVal}</Typography.Body1>
       </Box>
       <Box>
         <AccidentsOccurred singleType={singleType} accidentsCount={items.total_accidents_count} />
