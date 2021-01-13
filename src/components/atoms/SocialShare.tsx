69de2bc04f465ac35c06ad95fd75c4d26e23d049
Yael Luria <yaeluria@gmail.com>
2021-01-13 15:07:13 +0200

    convert styling to use Box

--- src/components/atoms/SocialShare.tsx
+++ src/components/atoms/SocialShare.tsx
@@ -1,28 +1,24 @@
 import React from 'react';
 import { FacebookShareButton, TwitterShareButton, FacebookIcon, TwitterIcon } from 'react-share';
-import { makeStyles } from '@material-ui/styles';
+import Box from '@material-ui/core/Box';
 
 const ICON_SIZE = 24;
 
-const useStyles = makeStyles({
-  root: {
-    '& *': {
-      padding: '6px 8px',
-    },
-  },
-});
 const SocialShare = () => {
-  const classes = useStyles();
   const url = window.location.origin;
   return (
-    <div className={classes.root}>
-      <FacebookShareButton url={url}>
-        <FacebookIcon round size={ICON_SIZE} />
-      </FacebookShareButton>
-      <TwitterShareButton url={url}>
-        <TwitterIcon round size={ICON_SIZE} />
-      </TwitterShareButton>
-    </div>
+    <Box display="flex">
+      <Box p={1}>
+        <FacebookShareButton url={url}>
+          <FacebookIcon round size={ICON_SIZE} />
+        </FacebookShareButton>
+      </Box>
+      <Box p={1}>
+        <TwitterShareButton url={url}>
+          <TwitterIcon round size={ICON_SIZE} />
+        </TwitterShareButton>
+      </Box>
+    </Box>
   );
 };
 export default SocialShare;
