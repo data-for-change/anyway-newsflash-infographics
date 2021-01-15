caf0566b98f8098d2cb2d399764f83e5728eafd9
danielsh28 <danielshely1@gmail.com>
2021-01-15 15:48:48 +0200

    Refactor 490 remove text at road (#566)
    * extract severity image to seperate component, add functionality to find single type and display severity image for that type, closes #447
    
    * refactor #504 textView
    
    * added regexp to cut road prefix
    
    * added regexp
    
    * removed redundant textview file
    
    Co-authored-by: Yael Luria <yaeluria@gmail.com>

--- src/components/molecules/TextView/TextViewHeader.tsx
+++ src/components/molecules/TextView/TextViewHeader.tsx
@@ -8,14 +8,17 @@ import { IWidgetCountBySeverityTextData } from '../../../models/WidgetData';
 interface IProps {
   data: IWidgetCountBySeverityTextData;
   segmentText: string;
+  singleType?: string;
 }
 interface AProps {
   accidentsCount: Number;
+  singleType: string | undefined;
 }
 
 const red = roadIconColors.red;
+const roadRegexp = /כביש \d+/;
 
-const AccidentsOccurred: FC<AProps> = ({ accidentsCount }) => {
+const AccidentsOccurred: FC<AProps> = ({ accidentsCount, singleType }) => {
   const { t, i18n } = useTranslation();
   const elements = [
     <Box mr={1} key={1}>
@@ -27,10 +30,17 @@ const AccidentsOccurred: FC<AProps> = ({ accidentsCount }) => {
     <Box mr={1} key={3}>
       <Typography.Body1>{t('textView.accidents')}</Typography.Body1>
     </Box>,
+    singleType && (
+      <Box mr={1} key={4}>
+        <Typography.Body1>
+          {accidentsCount > 1 ? t(`textView.${singleType}.plural`) : t(`textView.${singleType}.plural`)}{' '}
+        </Typography.Body1>
+      </Box>
+    ),
   ];
   // When the locale is English the last element needs to be first - x accidents occurred instead of occurred x accidents
-  const [a, b, c] = elements;
-  const elementsEnglish = [b, c, a];
+  const [a, b, c, d] = elements;
+  const elementsEnglish = [b, d, c, a];
   return (
     <Box display="flex" justifyContent="center">
       {i18n.language === 'he' ? elements : elementsEnglish}
@@ -38,8 +48,9 @@ const AccidentsOccurred: FC<AProps> = ({ accidentsCount }) => {
   );
 };
 
-const TextViewHeader: React.FC<IProps> = ({ data: { items }, segmentText }) => {
+const TextViewHeader: React.FC<IProps> = ({ data: { items }, segmentText, singleType }) => {
   const { t } = useTranslation();
+  const segmentPresentationVal: string = segmentText.replace(roadRegexp, '');
   return (
     <>
       {items.end_year === items.start_year ? (
@@ -56,10 +67,10 @@ const TextViewHeader: React.FC<IProps> = ({ data: { items }, segmentText }) => {
         </>
       )}
       <Box mr={1}>
-        <Typography.Body1>{t('textView.on') + segmentText}</Typography.Body1>
+        <Typography.Body1>{segmentPresentationVal}</Typography.Body1>
       </Box>
       <Box>
-        <AccidentsOccurred accidentsCount={items.total_accidents_count} />
+        <AccidentsOccurred singleType={singleType} accidentsCount={items.total_accidents_count} />
       </Box>
     </>
   );
--- src/components/molecules/TextView/TextViewRecord.tsx
+++ src/components/molecules/TextView/TextViewRecord.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Box from '@material-ui/core/Box';
 import { Typography } from '../../atoms';
 import { roadIconColors, silverSmokeColor } from '../../../style';
-import { makeStyles } from '@material-ui/core/styles';
+import SeverityImage from './SeverityImage';
 
 interface IProps {
   numOfAccidents: number;
@@ -12,20 +12,10 @@ interface IProps {
 
 const red = roadIconColors.red;
 
-const useStyles = makeStyles((theme) => ({
-  image: {
-    height: theme.spacing(11),
-    width: 'auto',
-  },
-}));
-
 const TextViewRecord: React.FC<IProps> = ({ numOfAccidents, severityDesc, imgSrc }) => {
-  const classes = useStyles();
   return (
     <Box display="flex" py={1} borderBottom={`5px solid ${silverSmokeColor}`}>
-      <Box flex={1} display="flex" justifyContent="center">
-        <img src={imgSrc} className={classes.image} alt="crutches" />
-      </Box>
+      <SeverityImage severity={imgSrc} />
       <Box flex={1} display="flex" justifyContent="center">
         <Box display="flex" flexDirection="column" alignItems="center">
           <Box color={red}>
