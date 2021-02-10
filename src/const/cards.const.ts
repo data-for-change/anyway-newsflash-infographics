9db67bc91ea4a90134b133f7c83e0e2af33acaee
yuvalbl <yuval.bar.levi@gmail.com>
2021-02-10 16:56:42 +0200

    hotfix getOperWidgetData

--- src/const/cards.const.ts
+++ src/const/cards.const.ts
@@ -1,7 +1,9 @@
+import { WidgetName } from '../models/WidgetName';
+
 // cards is this least will be avaiaable for operation environments (all non-staging environments)
-export const operationalCards = [
-  'accident_count_by_severity',
-  'most_severe_accidents_table',
-  'most_severe_accidents',
-  'head_on_collisions_comparison_percentage',
+export const operationalCards: WidgetName[] = [
+  WidgetName.accident_count_by_severity,
+  WidgetName.most_severe_accidents_table,
+  WidgetName.most_severe_accidents,
+  WidgetName.head_on_collisions_comparison_percentage,
 ];
