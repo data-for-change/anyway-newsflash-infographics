378e5611e9d32f928c5be4b8dc782edabb423027
sejoB <sejo1981@gmail.com>
2021-03-22 20:56:49 +0200

    fix header text view

--- src/components/molecules/card/CardHeader.tsx
+++ src/components/molecules/card/CardHeader.tsx
@@ -1,5 +1,6 @@
 import { Box } from '@material-ui/core';
 import React, { FC } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import { HeaderVariant } from '../../../services/widgets.style.service';
 import RoadNumberImage from './RoadNumberImage';
 import LamasImage from '../../../assets/cbs.png';
@@ -7,7 +8,25 @@ import AnywayImage from '../../../assets/anyway.png';
 import { Typography, Logo } from '../../atoms';
 import { useTranslation } from 'react-i18next';
 
-const MAX_WORDS_PER_TITLE_LINE = 5;
+const useStyles = makeStyles({
+  wrapper: {
+    width: '100%',
+    height: '100%',
+  },
+  textWrapper: {
+    width: '100%',
+  },
+  text: {
+    width: '75%',
+  },
+  roadImageWrapper: {
+    position: 'relative',
+    top: '40%',
+  },
+  logosContainer: {
+    height: '100%',
+  },
+});
 
 interface IProps {
   variant: HeaderVariant;
@@ -15,27 +34,20 @@ interface IProps {
   road: number;
 }
 const CardHeader: FC<IProps> = ({ variant, text, road }) => {
+  const classes = useStyles();
   const { i18n } = useTranslation();
 
   let headerContent = null;
-  let textLine1 = text;
-  let textLine2;
-  const textWordsArr = text?.split(' ');
-  const textWordLength = textWordsArr?.length;
-  if (textWordLength && textWordLength > MAX_WORDS_PER_TITLE_LINE) {
-    const indexToSplit = Math.floor(textWordLength / 2);
-    textLine1 = textWordsArr?.slice(0, indexToSplit).join(' ');
-    textLine2 = textWordsArr?.slice(indexToSplit).join(' ');
-  }
 
   switch (variant) {
     case HeaderVariant.Centered:
       headerContent = (
         <Box display="flex" alignItems="center" flex={1}>
           <RoadNumberImage roadNumber={road} />
-          <Box flex={1} px={2} textAlign="center">
-            <Typography.Body1>{textLine1} </Typography.Body1>
-            {textLine2 && <Typography.Body1>{textLine2}</Typography.Body1>}
+          <Box display="flex" justifyContent="center" px={2} className={classes.textWrapper}>
+            <Box className={classes.text}>
+              <Typography.Body1>{text}</Typography.Body1>
+            </Box>
           </Box>
         </Box>
       );
@@ -43,11 +55,11 @@ const CardHeader: FC<IProps> = ({ variant, text, road }) => {
     case HeaderVariant.Logo:
       headerContent = (
         <Box display="flex" flex={1}>
-          <Box position={'relative'} top={'40%'}>
+          <Box className={classes.roadImageWrapper}>
             <RoadNumberImage roadNumber={road} />
           </Box>
           <Box ml={'7%'} display="flex" flex={1} justifyContent="flex-end" alignItems="flex-end" height={30}>
-            <Box height={'100%'} m={i18n.language === 'he' ? '0 0 0 2.5%' : '0 2.5% 0 0'}>
+            <Box className={classes.logosContainer} m={i18n.language === 'he' ? '0 0 0 2.5%' : '0 2.5% 0 0'}>
               <Logo src={LamasImage} alt={'Lamas'} height={30} />
             </Box>
             <Logo src={AnywayImage} alt={'Anyway'} height={20} />
@@ -58,7 +70,7 @@ const CardHeader: FC<IProps> = ({ variant, text, road }) => {
   }
 
   return (
-    <Box height="100%" width="100%" display="flex">
+    <Box className={classes.wrapper} display="flex">
       {headerContent}
     </Box>
   );
