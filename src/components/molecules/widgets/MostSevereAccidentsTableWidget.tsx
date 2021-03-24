822b39bf3443043c79c6b74aa1511f06edc74386
sejoB <sejo1981@gmail.com>
2021-03-24 19:47:30 +0200

    in progress

--- src/components/molecules/widgets/MostSevereAccidentsTableWidget.tsx
+++ src/components/molecules/widgets/MostSevereAccidentsTableWidget.tsx
@@ -6,6 +6,22 @@ interface IProps {
   data: IWidgetMostSevereAccidentsTableData;
 }
 const MostSevereAccidentsTableWidget: FC<IProps> = ({ data }) => {
+  const { items } = data;
+  const labels = ['קל', 'קשה', 'הרוג', 'סוג תאונה', 'שעה', 'תאריך'];
+  const dataKeys = ['date', 'hour', 'type', 'killed_count', 'severe_injured_count', 'light_injured_count'];
+
+  const d = items.map((item: any, i) => {
+    return Object.keys(item)
+      .filter((key) => dataKeys.includes(key))
+      .reduce((obj, key) => {
+        return {
+          ...obj,
+          [key]: item[key],
+        };
+      }, {});
+  });
+  console.log('🚀 ~ file: MostSevereAccidentsTableWidget.tsx ~ line 24 ~ d ~ d', d);
+
   return <TableView data={data} />;
 };
 
