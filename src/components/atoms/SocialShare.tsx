60c0e14c7e916305226054ab270fca3b76b8a98f
Daniel Ilan <daniel.ilan222@gmail.com>
2021-04-08 17:21:08 +0300

    fixed social button outline

--- src/components/atoms/SocialShare.tsx
+++ src/components/atoms/SocialShare.tsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import { FacebookShareButton, TwitterShareButton, FacebookIcon, TwitterIcon } from 'react-share';
-import Box from '@material-ui/core/Box';
+import { Box, makeStyles } from '@material-ui/core/';
 
 const ICON_SIZE = 24;
 
+const useStyles = makeStyles({
+  noOutline: {
+    outline: 'none',
+  },
+});
+
 const SocialShare = () => {
+  const classes = useStyles();
   const url = window.location.origin;
   return (
     <Box display="flex">
       <Box p={1}>
-        <FacebookShareButton url={url}>
+        <FacebookShareButton url={url} className={classes.noOutline}>
           <FacebookIcon round size={ICON_SIZE} />
         </FacebookShareButton>
       </Box>
