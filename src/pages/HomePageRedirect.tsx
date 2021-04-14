c7fd6a55d053167f2a21912fa92bf5902846b0ed
Daniel Shely <danielshely1@gmail.com>
2021-04-14 15:34:34 +0300

    remove unnecessary query param

--- src/pages/HomePageRedirect.tsx
+++ src/pages/HomePageRedirect.tsx
@@ -3,16 +3,14 @@ import { Redirect } from 'react-router';
 import { observer } from 'mobx-react-lite';
 import { useStore } from '../store/storeConfig';
 import RootStore from '../store/root.store';
-import { useQuery } from '../utils/utils';
 
 interface IProps {}
 
 // Used to redirect from root page to latest newsflash
 const HomePageRedirect: FC<IProps> = () => {
   const store: RootStore = useStore();
-  const isUpdateUser = useQuery().get('updateUser') === 'true';
   const defaultId = store?.newsFlashCollection[0]?.id;
-  return defaultId ? <Redirect to={`/newsflash/${defaultId}${isUpdateUser?'?updateUser=true' : ''}`} /> : null;
+  return defaultId ? <Redirect to={`/newsflash/${defaultId}`} /> : null;
 };
 
 export default observer(HomePageRedirect);
