8461525eea5338b609027c5c3729541d839725a6
Idan <eidan47@gmail.com>
2021-04-28 21:38:16 +0300

    [feat-616] use useStyles hook for the style

--- src/components/atoms/Typography.tsx
+++ src/components/atoms/Typography.tsx
@@ -1,5 +1,6 @@
 import React, { FC } from 'react';
 import Typo from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
 
 interface IBold {
   bold?: boolean;
@@ -8,47 +9,56 @@ interface IText {
   [property: string]: FC<IBold>;
 }
 
-const style = { fontWeight: 'bold' } as const;
+const useStyles = makeStyles(() => ({
+  bold: {
+    fontWeight: 'bold',
+  },
+}));
+
+const useBold = (bold: boolean) => {
+  const classes = useStyles();
+  return bold ? classes.bold : '';
+};
 
 // for styles of each variant - see 'theme.ts'
 const Typography: IText = {
   Title1: ({ children }, bold) => (
-    <Typo style={bold && style} variant="subtitle1" component="h2">
+    <Typo className={useBold(bold)} variant="subtitle1" component="h2">
       {children}
     </Typo>
   ),
   Title2: ({ children }, bold) => (
-    <Typo style={bold && style} variant="subtitle2" component="h3">
+    <Typo className={useBold(bold)} variant="subtitle2" component="h3">
       {children}
     </Typo>
   ),
   Body1: ({ children }, bold) => (
-    <Typo style={bold && style} variant="h1" component="span">
+    <Typo className={useBold(bold)} variant="h1" component="span">
       {children}
     </Typo>
   ),
   Body2: ({ children }, bold) => (
-    <Typo style={bold && style} variant="h2" component="span">
+    <Typo className={useBold(bold)} variant="h2" component="span">
       {children}
     </Typo>
   ),
   Body3: ({ children }, bold) => (
-    <Typo style={bold && style} variant="h3" component="span">
+    <Typo className={useBold(bold)} variant="h3" component="span">
       {children}
     </Typo>
   ),
   Body4: ({ children }, bold) => (
-    <Typo style={bold && style} variant="h4" component="span">
+    <Typo className={useBold(bold)} variant="h4" component="span">
       {children}
     </Typo>
   ),
   Body5: ({ children }, bold) => (
-    <Typo style={bold && style} variant="h5" component="span">
+    <Typo className={useBold(bold)} variant="h5" component="span">
       {children}
     </Typo>
   ),
   Body6: ({ children }, bold) => (
-    <Typo style={bold && style} variant="h6" component="span">
+    <Typo className={useBold(bold)} variant="h6" component="span">
       {children}
     </Typo>
   ),
