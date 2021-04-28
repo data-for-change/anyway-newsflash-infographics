6900527d9ba19967ce1d5eb7c9b0916eab0c7599
Idan <eidan47@gmail.com>
2021-04-28 14:01:20 +0300

    [feat-616] add bold to typhography atom

--- src/components/atoms/Typography.tsx
+++ src/components/atoms/Typography.tsx
@@ -1,49 +1,54 @@
 import React, { FC } from 'react';
 import Typo from '@material-ui/core/Typography';
 
-interface IText {
-  [property: string]: FC;
+interface IBold {
+  bold?: boolean;
 }
+interface IText {
+  [property: string]: FC<IBold>;
+}
+
+const style = { fontWeight: 'bold' } as const;
 
 // for styles of each variant - see 'theme.ts'
 const Typography: IText = {
-  Title1: ({ children }) => (
-    <Typo variant="subtitle1" component="h2">
+  Title1: ({ children }, bold) => (
+    <Typo style={bold && style} variant="subtitle1" component="h2">
       {children}
     </Typo>
   ),
-  Title2: ({ children }) => (
-    <Typo variant="subtitle2" component="h3">
+  Title2: ({ children }, bold) => (
+    <Typo style={bold && style} variant="subtitle2" component="h3">
       {children}
     </Typo>
   ),
-  Body1: ({ children }) => (
-    <Typo variant="h1" component="span">
+  Body1: ({ children }, bold) => (
+    <Typo style={bold && style} variant="h1" component="span">
       {children}
     </Typo>
   ),
-  Body2: ({ children }) => (
-    <Typo variant="h2" component="span">
+  Body2: ({ children }, bold) => (
+    <Typo style={bold && style} variant="h2" component="span">
       {children}
     </Typo>
   ),
-  Body3: ({ children }) => (
-    <Typo variant="h3" component="span">
+  Body3: ({ children }, bold) => (
+    <Typo style={bold && style} variant="h3" component="span">
       {children}
     </Typo>
   ),
-  Body4: ({ children }) => (
-    <Typo variant="h4" component="span">
+  Body4: ({ children }, bold) => (
+    <Typo style={bold && style} variant="h4" component="span">
       {children}
     </Typo>
   ),
-  Body5: ({ children }) => (
-    <Typo variant="h5" component="span">
+  Body5: ({ children }, bold) => (
+    <Typo style={bold && style} variant="h5" component="span">
       {children}
     </Typo>
   ),
-  Body6: ({ children }) => (
-    <Typo variant="h6" component="span">
+  Body6: ({ children }, bold) => (
+    <Typo style={bold && style} variant="h6" component="span">
       {children}
     </Typo>
   ),
