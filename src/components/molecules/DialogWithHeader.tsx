782ec758163bec04279aa91a3ed048992f20fe6c
danielsh28 <danielshely1@gmail.com>
2021-05-05 18:13:25 +0300

    Feat-467 user user registration screen (#619)
    * upload for draft pr
    
    * first success signed in display using email as identifier
    
    * add auth server url env var
    
    * added pop-up ridirect and button
    
    * added pop-up ridirect and button
    
    * initial flow of screen and user update
    
    * aded i18 and refactor update screen flow
    
    * basic flow for sign in
    
    * remove unnecessary query param
    
    * apply destructuring to form event
    
    * changes:
     logout api call added to user.service.ts
     destructuring for orm default values applied in Header.tsx applied
     another import was added
    
    * fix import to one liner
    
    * added fixes following review

--- src/components/molecules/DialogWithHeader.tsx
+++ src/components/molecules/DialogWithHeader.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from 'react';
 import { Dialog, Typography } from '../atoms';
-import { Box, makeStyles, createStyles, Theme, IconButton, DialogTitle } from '@material-ui/core';
+import { Box, makeStyles, createStyles, Theme, IconButton, DialogTitle, DialogContent } from '@material-ui/core';
 import CloseIcon from '@material-ui/icons/Close';
 import { shadowColor, blueVioletColor } from '../../style';
 
@@ -28,6 +28,9 @@ const useStyles = makeStyles((theme: Theme) =>
     close: {
       color: shadowColor,
     },
+    content: {
+      overflow: 'hidden',
+    },
   }),
 );
 
@@ -43,9 +46,7 @@ const DialogWithHeader: FC<IProps> = ({ onClose, isShowing, title, fullWidth, ch
           </IconButton>
         </Box>
       </DialogTitle>
-      <Box display="flex" flexDirection="column" px={3} py={3}>
-        {children}
-      </Box>
+      <DialogContent className={classes.content}>{children}</DialogContent>
     </Dialog>
   );
 };
