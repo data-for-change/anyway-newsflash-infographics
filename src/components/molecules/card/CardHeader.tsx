ce5a83705093aad2e22cd61d41780e2cc7de6fdd
sejoB <sejo1981@gmail.com>
2021-05-05 19:50:13 +0300

    breake card header title

--- src/components/molecules/card/CardHeader.tsx
+++ src/components/molecules/card/CardHeader.tsx
@@ -7,6 +7,7 @@ import AnywayImage from '../../../assets/anyway.png';
 import { Typography, Logo } from '../../atoms';
 import { useTranslation } from 'react-i18next';
 import { silverSmokeColor, opacity80percent } from '../../../style/_globals';
+import { splitTextHeader } from '../../../utils/string.utils';
 
 const useStyles = makeStyles({
   wrapper: {
@@ -17,7 +18,7 @@ const useStyles = makeStyles({
     width: '100%',
   },
   text: {
-    width: '75%',
+    maxWidth: '75%',
   },
   roadImageWrapper: {
     position: 'relative',
@@ -49,14 +50,17 @@ const CardHeader: FC<IProps> = ({ variant, text, road }) => {
 
   let headerContent = null;
 
+  const headerText = splitTextHeader(text);
+
   switch (variant) {
     case HeaderVariant.Centered:
       headerContent = (
         <Box display="flex" alignItems="center" flex={1}>
           <RoadNumberImage roadNumber={road} />
           <Box display="flex" justifyContent="center" px={2} className={classes.textWrapper}>
-            <Box className={classes.text}>
-              <Typography.Body1>{text}</Typography.Body1>
+            <Box display="flex" flexDirection="column" className={classes.text}>
+              <Typography.Body1>{headerText?.textLine1}</Typography.Body1>
+              <Typography.Body1>{headerText?.textLine2}</Typography.Body1>
             </Box>
           </Box>
         </Box>
