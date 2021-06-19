2d71a81601bd55bf3f461081a9fa29bdda8fa8dd
Daniel Ilan <daniel.ilan222@gmail.com>
2021-06-19 18:54:20 +0300

    deleted redundent code

--- src/components/molecules/card/CardFooter.tsx
+++ src/components/molecules/card/CardFooter.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Logo, Typography } from '../../atoms';
 import LamasImage from '../../../assets/cbs.png';
 import AnywayImage from '../../../assets/anyway.png';
