f6c77da70fcfbd335dc802e9e4da9449e61b3c44
Daniel Ilan <daniel.ilan222@gmail.com>
2021-06-19 17:42:14 +0300

    render text in SVG

--- src/components/molecules/card/CardFooter.tsx
+++ src/components/molecules/card/CardFooter.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Logo, Typography } from '../../atoms';
 import LamasImage from '../../../assets/cbs.png';
 import AnywayImage from '../../../assets/anyway.png';
 import { makeStyles } from '@material-ui/core/styles';
 import { cardFooterHeight } from '../../../style';
 import { Box } from '@material-ui/core';
+import { useStore } from '../../../store/storeConfig';
 
 interface IProps {
   dateComment: string;
@@ -20,16 +21,28 @@ const useStyles = makeStyles({
     justifyContent: 'flex-end',
   },
   comment: {
-    paddingLeft: '1rem',
+    display: 'block',
   },
 });
 
+type SvgInHtml = HTMLElement & SVGTextElement;
+
 const CardFooter: React.FC<IProps> = ({ dateComment }) => {
   const classes = useStyles();
+  const store = useStore();
+  const [xPosition, setXPosition] = useState(300);
+  useEffect(() => {
+    setXPosition(store.currentLanguageRouteString === '/en' ? 0 : 300);
+  }, [store.currentLanguageRouteString]);
+
   return (
     <div className={classes.main}>
       <Typography.Body3>
-        <div className={classes.comment}>{dateComment}</div>
+        <svg className={classes.comment} width="100%" height="50" xmlns="http://www.w3.org/2000/svg">
+          <text y="90%" x={xPosition}>
+            {dateComment}
+          </text>
+        </svg>
       </Typography.Body3>
       <Box display="flex" flex={1} />
       <Logo src={LamasImage} alt={'Lamas'} height={30} />
