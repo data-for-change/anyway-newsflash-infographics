77ba2acaca5b5b07bb1ebb2c06b23d41f03d1f46
Daniel Ilan <daniel.ilan222@gmail.com>
2021-06-26 14:42:18 +0300

    switched to use only html-to-image package

--- src/components/molecules/card/CardFooter.tsx
+++ src/components/molecules/card/CardFooter.tsx
@@ -1,11 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Logo, Typography } from '../../atoms';
 import LamasImage from '../../../assets/cbs.png';
 import AnywayImage from '../../../assets/anyway.png';
 import { makeStyles } from '@material-ui/core/styles';
 import { cardFooterHeight } from '../../../style';
 import { Box } from '@material-ui/core';
-import { useStore } from '../../../store/storeConfig';
 
 interface IProps {
   dateComment: string;
@@ -27,21 +26,10 @@ const useStyles = makeStyles({
 
 const CardFooter: React.FC<IProps> = ({ dateComment }) => {
   const classes = useStyles();
-  const store = useStore();
-  const [xPosition, setXPosition] = useState(300);
-  useEffect(() => {
-    setXPosition(store.currentLanguageRouteString === '/en' ? 0 : 300);
-  }, [store.currentLanguageRouteString]);
 
   return (
     <div className={classes.main}>
-      <Typography.Body3>
-        <svg className={classes.comment} width="100%" height="50" xmlns="http://www.w3.org/2000/svg">
-          <text y="90%" x={xPosition}>
-            {dateComment}
-          </text>
-        </svg>
-      </Typography.Body3>
+      <Typography.Body3>{dateComment}</Typography.Body3>
       <Box display="flex" flex={1} />
       <Logo src={LamasImage} alt={'Lamas'} height={30} />
       <Logo src={AnywayImage} alt={'Anyway'} height={20} />
