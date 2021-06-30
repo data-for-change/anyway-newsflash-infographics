6abdf9ff7d6170d6f41a25c8fd6e89cf28fea989
Yuval <yuvalbl@users.noreply.github.com>
2021-06-30 19:35:33 +0300

    add feature flags support (#698)

---
+++ src/utils/env.utils.ts
@@ -0,0 +1,7 @@
+﻿export const isProd = process.env.NODE_ENV === 'production';
+
+// temporary helpers for features flags.
+// once feature is production ready - method and env var can be deleted
+export const FEATURE_FLAGS = {
+  login: !isProd || process.env.REACT_APP_LOGIN_READY_FOR_PROD === 'true',
+};
