d68dc06d5da0df8d97a8f1bd22bc610db59db3d3
noa <noahananel2@gmail.com>
2021-08-18 19:21:20 +0300

    Fixing useEffect related build fail

--- src/hooks/date.hooks.ts
+++ src/hooks/date.hooks.ts
@@ -13,6 +13,6 @@ export function useLocaleValue() {
 
     useEffect(() => {
         setLang(store.selectedLanguage);
-    });
+    }), [store.selectedLanguage];
     return lang === 'he' ? locales['hebrew'] : locales['english'];
 }
