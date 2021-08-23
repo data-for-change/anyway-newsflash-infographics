fc97326bd4beb05398ccb4a34f1ba8446bc4da97
SejoB <sejo1981@gmail.com>
2021-08-23 20:24:23 +0300

    location component

---
+++ src/components/molecules/LocationSelect.tsx
@@ -0,0 +1,40 @@
+import { FC } from 'react';
+import { MapContainer, useMapEvents } from 'react-leaflet';
+import { IPoint } from 'models/Point';
+import { INITIAL_CENTER } from 'utils/utils';
+import { Marker } from 'atoms';
+import GoogleMapsLayer from './map/GoogleMapsLayer';
+
+const INITIAL_ZOOM = parseInt(process.env.REACT_APP_DEFAULT_MAP_ZOOM!);
+
+interface ILocation {
+  onLocationChange: (location: IPoint) => void;
+}
+
+const LocationPicker: FC<ILocation> = ({ onLocationChange }) => {
+  useMapEvents({
+    click: (event) => {
+      const {latlng: { lng, lat }} = event;
+      onLocationChange({ longitude: lng,latitude: lat });
+    },
+  });
+  return null;
+};
+
+interface IProps {
+  location: IPoint | null;
+  onLocationChange: (location: IPoint | null) => void;
+}
+//TODO create Map component
+const LocationSelect:FC<IProps> = ({location, onLocationChange}) => {
+
+  return (
+    <MapContainer center={INITIAL_CENTER} zoom={INITIAL_ZOOM} style={{ height: '100%', width: '100%' }}>
+      <LocationPicker onLocationChange={onLocationChange} />
+      <GoogleMapsLayer />
+      {location && <Marker markerdata={location} />}
+    </MapContainer>
+  );
+};
+
+export default LocationSelect;
--- src/services/data.verification/data.verification.service.ts
+++ src/services/data.verification/data.verification.service.ts
@@ -1,7 +1,6 @@
 import { verifiedWidgetData } from './data.verification';
 
 export function getVerifiedWidgetsData(widgets: Array<any>) {
-  console.log('🚀 ~ file: data.verification.service.ts ~ line 4 ~ getVerifiedWidgetsData ~ widgets', widgets);
   const verifiedWidgets = widgets.filter((widget, index) => {
     // general structure tests (for all widgets)
     // test name property
