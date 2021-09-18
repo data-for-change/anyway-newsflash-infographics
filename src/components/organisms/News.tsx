6f56b266f157776eb8db085abfe7846362b7eb62
noa-ui <64693838+noa-ui@users.noreply.github.com>
2021-09-18 23:15:37 +0300

    [FIX-597] Adapted sidebar date format according to the site language (#739)
    * Adapted sidebar date format according to the site language, table date format is gotten as a string - cannot adapt it as well
    
    * Changed date formatting to be from a func
    
    * Changed change-languate's date logic into a hook
    
    * Delete PopUpRedirect.tsx
    
    * Fixed no newlineat end of file, changed to object for readability
    
    * Fixing build issues
    
    * Fixing useEffect related build fail
    
    * Fixing usEffect build fail
    
    * Added default to switch case in date hook, removed temp date formatting in cards
    
    * removed temp date formatting in cards
    
    * Delete package-lock.json
    
    * Delete package.json
    
    * Added accidently deleted package files
    
    * Modified import into absolute and replaced to ''
    
    * Removed non identical package files
    
    * Added the package.json files from dev branch
    
    * converted let vars to const, Changed useLocale hook
    
    * Reverted unknown changes to the updated file with the changes
    
    * Update src/utils/time.utils.ts
    
    Changed  "" -> ''
    
    Co-authored-by: Yuval <yuvalbl@users.noreply.github.com>
    
    Co-authored-by: noa <noahananel2@gmail.com>
    Co-authored-by: Yuval <yuvalbl@users.noreply.github.com>

--- src/components/organisms/News.tsx
+++ src/components/organisms/News.tsx
@@ -5,6 +5,8 @@ import { silverSmokeColor } from 'style';
 import { useStore } from 'store/storeConfig';
 import RootStore from 'store/root.store';
 import { observer } from 'mobx-react-lite';
+import { dateFormat } from 'utils/time.utils';
+import { useLocale } from 'hooks/date.hooks';
 
 const useStyles = makeStyles({
   container: {},
@@ -19,6 +21,7 @@ const useStyles = makeStyles({
 const News: FC = () => {
   const store: RootStore = useStore();
   const classes = useStyles();
+  const locale = useLocale();
 
   return (
     <Box flexGrow={1} display="flex" flexDirection="column" className={classes.newsFeed}>
@@ -27,7 +30,7 @@ const News: FC = () => {
           {store.newsFlashCollection.length > 0 ? (
             store.newsFlashCollection.map((news) => {
               const className = news.id === store.activeNewsFlashId ? classes.activeNewsFlash : '';
-              const date = news.date == null ? '' : new Date(news.date.replace(/-/g, '/')).toLocaleDateString();
+              const date = news.date == null ? '' : dateFormat(new Date(news.date.replace(/-/g, '/')), locale);
               return (
                 <Link key={news.id} to={`${store.currentLanguageRouteString}/newsflash/${news.id}`}>
                   <Box border={1} borderColor={silverSmokeColor} p={1} className={className}>
