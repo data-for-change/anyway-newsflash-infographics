588a94c0b3b10530d98ed5156293dcbbdecaf6a9
Shlomi Baruch <shlomibaruch5@gmail.com>
2021-09-29 00:57:10 +0300

    data service fetchWidgets

--- src/services/widgets.data.service.ts
+++ src/services/widgets.data.service.ts
@@ -8,54 +8,55 @@ import { getVerifiedWidgetsData } from './data.verification/data.verification.se
 const NEWS_FLASH_API: string = '/api/infographics-data';
 
 export const fetchWidgets = async (id: number, lang: string, yearAgo?: number): Promise<ILocationData | undefined> => {
-  if (showDemoCards && id === DEMO_ID) {
-    return getDemoWidgetData();
-  }
+	if (showDemoCards && id === DEMO_ID) {
+		return getDemoWidgetData();
+	}
 
-  try {
-    const query = [`lang=${lang}&news_flash_id=${id}`];
-    if (yearAgo) {
-      query.push(`years_ago=${yearAgo}`);
-    }
-    if (SHOW_MOCK) {
-      query.push(`mock=${SHOW_MOCK}`);
-    }
-
-  const widgetsUrl = `${NEWS_FLASH_API}?${query.join('&')}`;    //temp - long response time of server- keep console.log to see out url
-    console.log(widgetsUrl);
-    const response = await axios.get(widgetsUrl);
-    return processWidgetsFetchResponse(response);
-  } catch (error) {
-    console.error(error);
-  }
+	try {
+		const query = [
+			`lang=${lang}&news_flash_id=${id}`
+		];
+		if (yearAgo) {
+			query.push(`years_ago=${yearAgo}`);
+		}
+		if (SHOW_MOCK) {
+			query.push(`mock=${SHOW_MOCK}`);
+		}
+  
+		const widgetsUrl = `${NEWS_FLASH_API}?${query.join('&')}`;
+		const response = await axios.get(widgetsUrl);
+		return processWidgetsFetchResponse(response);
+	} catch (error) {
+		console.error(error);
+	}
 };
 
 function processWidgetsFetchResponse(response: any) {
-  const meta = response.data.meta;
-  let widgets = response.data.widgets;
-  widgets = getVerifiedWidgetsData(widgets);
-  widgets = addWidgetsVariants(widgets);
+	const meta = response.data.meta;
+	let widgets = response.data.widgets;
+	widgets = getVerifiedWidgetsData(widgets);
+	widgets = addWidgetsVariants(widgets);
 
-  if (showOnlyOperCards) {
-    widgets = getOperWidgetData(widgets);
-  }
-  return { meta, widgets };
+	if (showOnlyOperCards) {
+		widgets = getOperWidgetData(widgets);
+	}
+	return { meta, widgets };
 }
 
 function getOperWidgetData(widgets: Array<any>) {
-  return widgets.filter((widget) => operationalCards.includes(widget.name));
+	return widgets.filter((widget) => operationalCards.includes(widget.name));
 }
 
 // for future use
 function addWidgetsVariants(widgets: Array<IWidgetBase>) {
-  const index = widgets.findIndex((w) => w.name === 'head_on_collisions_comparison');
-  if (index >= 0) {
-    // create variant
-    const widget = widgets[index];
-    const widgetVariant = { ...widget };
-    widgetVariant.name = widget.name + '_percentage';
-    // add variant after original widget
-    widgets.splice(index, 0, widgetVariant);
-  }
-  return widgets;
+	const index = widgets.findIndex((w) => w.name === 'head_on_collisions_comparison');
+	if (index >= 0) {
+		// create variant
+		const widget = widgets[index];
+		const widgetVariant = { ...widget };
+		widgetVariant.name = widget.name + '_percentage';
+		// add variant after original widget
+		widgets.splice(index, 0, widgetVariant);
+	}
+	return widgets;
 }
