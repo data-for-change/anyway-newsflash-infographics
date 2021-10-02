3dc039811981bae9012d4ca2faa9ddbc51150c86
SejoB <39242962+SejoB@users.noreply.github.com>
2021-10-02 20:12:32 +0800

    feat-687-location-picker-component (#754)
    * location component
    
    * fix imports
    
    * generic map component
    
    * remove space
    
    * requested changes

---
+++ src/components/molecules/LocationSelect.tsx
@@ -0,0 +1,37 @@
+import { FC } from 'react';
+import { useMapEvents } from 'react-leaflet';
+import { IPoint } from 'models/Point';
+import { Marker } from 'components/atoms';
+import GoogleMapsLayer from './map/GoogleMapsLayer';
+import Map from './map/Map';
+
+interface ILocation {
+  onLocationChange: (location: IPoint) => void;
+}
+
+const LocationPicker: FC<ILocation> = ({ onLocationChange }) => {
+  useMapEvents({
+    click: (event) => {
+      const {latlng: { lng, lat }} = event;
+      onLocationChange({ longitude: lng,latitude: lat });
+    },
+  });
+  return null;
+};
+
+interface IProps {
+  location: IPoint | null;
+  onLocationChange: (location: IPoint | null) => void;
+}
+
+const LocationSelect: FC<IProps> = ({ location, onLocationChange }) => {
+  return (
+    <Map>
+      <LocationPicker onLocationChange={onLocationChange} />
+      <GoogleMapsLayer />
+      {location && <Marker markerdata={location} />}
+    </Map>
+  );
+};
+
+export default LocationSelect;
