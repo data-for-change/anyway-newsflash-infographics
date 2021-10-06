4180ff74587e1e1036ce5d4852eced63b7fbff71
chen.m <chen.m@taboola.com>
2021-10-06 20:57:26 +0300

    fix-773 reformatted with prettier linter

--- src/components/atoms/Typography.tsx
+++ src/components/atoms/Typography.tsx
@@ -22,42 +22,42 @@ const useBold = (bold: boolean) => {
 
 // for styles of each variant - see 'theme.ts'
 const Typography: IText = {
-  Title1: ({ children , bold= false}) => (
+  Title1: ({ children, bold = false }) => (
     <Typo className={useBold(bold)} variant="subtitle1" component="h2">
       {children}
     </Typo>
   ),
-  Title2: ({ children , bold= false}) => (
+  Title2: ({ children, bold = false }) => (
     <Typo className={useBold(bold)} variant="subtitle2" component="h3">
       {children}
     </Typo>
   ),
-  Body1: ({ children , bold= false}) => (
+  Body1: ({ children, bold = false }) => (
     <Typo className={useBold(bold)} variant="h1" component="span">
       {children}
     </Typo>
   ),
-  Body2: ({ children , bold= false}) => (
+  Body2: ({ children, bold = false }) => (
     <Typo className={useBold(bold)} variant="h2" component="span">
       {children}
     </Typo>
   ),
-  Body3: ({ children , bold= false}) => (
+  Body3: ({ children, bold = false }) => (
     <Typo className={useBold(bold)} variant="h3" component="span">
       {children}
     </Typo>
   ),
-  Body4: ({ children , bold= false}) => (
+  Body4: ({ children, bold = false }) => (
     <Typo className={useBold(bold)} variant="h4" component="span">
       {children}
     </Typo>
   ),
-  Body5: ({ children , bold= false}) => (
+  Body5: ({ children, bold = false }) => (
     <Typo className={useBold(bold)} variant="h5" component="span">
       {children}
     </Typo>
   ),
-  Body6: ({ children , bold= false}) => (
+  Body6: ({ children, bold = false }) => (
     <Typo className={useBold(bold)} variant="h6" component="span">
       {children}
     </Typo>
