87fba840fe3a96307a58bb3c133e77a1d4c791e9
Yuval <yuvalbl@users.noreply.github.com>
2021-10-13 22:26:46 +0300

    Add global error handler (#797)
    * app-level error boundry
    
    * add global error handler

--- src/index.tsx
+++ src/index.tsx
@@ -20,3 +20,16 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+// treat unhandled errors
+// see https://developer.mozilla.org/en-US/docs/Web/API/GlobalEventHandlers/onerror
+// https://stackoverflow.com/questions/64318300/react-global-error-handler-not-working-for-async-componentdidmount-tried-both-c/64319415#64319415
+window.addEventListener('error', function (event) {
+  console.error(event);
+  // add logs server - 796
+});
+
+window.onunhandledrejection = (e: PromiseRejectionEvent) => {
+  console.error(e);
+  throw new Error(e.reason.stack);
+};
