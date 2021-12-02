a0f33d1078dd7ad1f3355ec6aa640df4cc1959d6
harduf laor <harduf93@gmail.com>
2021-12-02 10:43:14 +0200

    added last updated in card footer, and a 'last updated' in translation.json, but date info from backend isn't fixed yet

--- src/components/molecules/card/CardFooter.tsx
+++ src/components/molecules/card/CardFooter.tsx
@@ -6,6 +6,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import { cardFooterHeight } from 'style';
 import { Box } from '@material-ui/core';
 import { IDateComments } from 'models/WidgetData';
+import { useTranslation } from 'react-i18next';
 
 interface IProps {
   dateComment: IDateComments;
@@ -20,14 +21,33 @@ const useStyles = makeStyles({
     alignItems: 'flex-end',
     justifyContent: 'flex-end',
   },
+  spaceSpan: {
+    marginLeft: '4px',
+    marginRight: '4px',
+  },
 });
 
+const generateDate = (dateObj: any) => {
+  const lastDate = new Date(dateObj);
+  const year = lastDate.getFullYear();
+  const day = lastDate.getDate();
+  const month = lastDate.getMonth();
+
+  return `${day}/${month + 1}/${year}`;
+};
+
 const CardFooter: React.FC<IProps> = ({ dateComment }) => {
+  const { t } = useTranslation();
   const classes = useStyles();
-  const dateRange = dateComment?.date_range?.join('-') ?? ''
+  const lastDateUpdated = generateDate(dateComment.last_update);
+  const dateRange = dateComment?.date_range?.join('-') ?? '';
   return (
     <div className={classes.main}>
-      <Typography.Body3>{dateRange}</Typography.Body3>
+      <Typography.Body3>{dateRange},</Typography.Body3>
+      <Typography.Body5>
+        <span className={classes.spaceSpan}>{t('widgets.lastDateUpdated')}:</span>
+      </Typography.Body5>
+      <Typography.Body3>{lastDateUpdated}</Typography.Body3>
       <Box display="flex" flex={1} />
       <Logo src={LamasImage} alt={'Lamas'} height={30} />
       <Logo src={AnywayImage} alt={'Anyway'} height={20} />
