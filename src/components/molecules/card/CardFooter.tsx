d6742d4628581c691c616bf59644ea3c4fed1049
harduf laor <harduf93@gmail.com>
2021-12-09 12:40:13 +0200

    using prepared dateFormat function, instead of self implemenation

--- src/components/molecules/card/CardFooter.tsx
+++ src/components/molecules/card/CardFooter.tsx
@@ -7,6 +7,8 @@ import { cardFooterHeight } from 'style';
 import { Box } from '@material-ui/core';
 import { IDateComments } from 'models/WidgetData';
 import { useTranslation } from 'react-i18next';
+import { dateFormat } from 'utils/time.utils';
+import { useLocale } from 'hooks/date.hooks';
 
 interface IProps {
   dateComment: IDateComments;
@@ -21,25 +23,22 @@ const useStyles = makeStyles({
     alignItems: 'flex-end',
     justifyContent: 'flex-end',
   },
-  spaceSpan: {
-    marginLeft: '4px',
-    marginRight: '4px',
-  },
 });
 
 const CardFooter: React.FC<IProps> = ({ dateComment }) => {
   const { t } = useTranslation();
   const classes = useStyles();
-  const lastDate = new Date(dateComment.last_update);
-  const lastDateString = dateComment.last_update? `${lastDate.getDate()}/${lastDate.getMonth() + 1}/${lastDate.getFullYear()}`: ""
-  const dateRange = dateComment?.date_range?.join('-') ?? '';
+  const locale = useLocale();
+  const lastDate = dateComment.last_update == null ? '' : dateFormat(new Date(dateComment.last_update), locale);
+  const dateRange = dateComment.date_range == null ? '' : dateComment.date_range.join('-')
   return (
     <div className={classes.main}>
-      <Typography.Body3>{dateRange},</Typography.Body3>
-      <Typography.Body3>
-        <span className={classes.spaceSpan}>{t('widgets.lastDateUpdated')}:</span>
-      </Typography.Body3>
-      <Typography.Body3>{lastDateString}</Typography.Body3>
+      <Typography.Body3>{dateRange} </Typography.Body3>
+      {lastDate && (
+        <Typography.Body3>
+          ,{t('widgets.lastDateUpdated')}: {lastDate}
+        </Typography.Body3>
+      )}
       <Box display="flex" flex={1} />
       <Logo src={LamasImage} alt={'Lamas'} height={30} />
       <Logo src={AnywayImage} alt={'Anyway'} height={20} />
