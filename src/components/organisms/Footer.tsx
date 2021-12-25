b78fdd7a901b9f4b4a61f5e33dd2ae713ca8c0df
Yuval <yuvalbl@users.noreply.github.com>
2021-12-25 14:27:30 +0200

    [Feat-776] change lint script (#831)
    * change lint script
    
    * add no-restricted-imports rule to avoid relative imports
    
    * add no-multiple-empty-lines error
    
    * fixed according to npm run lint suggestions
    
    Co-authored-by: harduf <harduf93@gmail.com>

--- src/components/organisms/Footer.tsx
+++ src/components/organisms/Footer.tsx
@@ -7,7 +7,7 @@ import { Typography } from 'components/atoms';
 import { silverSmokeColor, oceanBlueColor, skyBlueColor } from 'style';
 import ThankYouDialog from './ThankYouDialog';
 import AboutDialog from './AboutDialog';
-import { version } from '../../../package.json';
+import { version } from '../../../package.json'; // eslint-disable-line
 
 interface IProps {}
 const useStyles = makeStyles({
