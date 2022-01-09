4dd80968f3d3c7df97a045588d5645b481aa9bde
harduf <harduf93@gmail.com>
2022-01-09 21:36:38 +0200

    removing not needed useEffect

--- src/components/molecules/AdminManagementForm.tsx
+++ src/components/molecules/AdminManagementForm.tsx
@@ -1,5 +1,5 @@
 import { observer } from 'mobx-react-lite';
-import React, { useEffect } from 'react';
+import React from 'react';
 import DialogWithHeader from './DialogWithHeader';
 import { useTranslation } from 'react-i18next';
 import { makeStyles, withStyles, createStyles } from '@material-ui/core/styles';
