59a275e06c92f501479d8dc51b0576d71af3a03c
Itai <itai.ozerii@gmail.com>
2022-06-12 18:18:28 +0300

    updated injured widget

--- src/components/molecules/widgets/WidgetWrapper.tsx
+++ src/components/molecules/widgets/WidgetWrapper.tsx
@@ -68,9 +68,7 @@ const WidgetWrapper: FC<IProps> = ({ widget, locationText, sizeOptions, segmentT
       break;
     }
     case WidgetName.accidents_heat_map: {
-      widgetComponent = (
-        <HeatMap data={data.items as IPoint[]} />
-      );
+      widgetComponent = <HeatMap data={data.items as IPoint[]} />;
       break;
     }
     // remove street_view until fixed
@@ -131,7 +129,7 @@ const WidgetWrapper: FC<IProps> = ({ widget, locationText, sizeOptions, segmentT
       break;
     }
     case WidgetName.injured_count_by_accident_year: {
-      widgetComponent = <CountInjuredByYearBarWidget data={data as IWidgetInjuredByYearData} />;
+      widgetComponent = <CountInjuredByYearBarWidget data={data as IWidgetMultiBarData} />;
       break;
     }
     case WidgetName.accident_count_by_day_night: {
