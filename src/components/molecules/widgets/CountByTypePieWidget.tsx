dbdfa2d6e5ed9b8a5d9219e22a7987a493941766
Harduf-l <88275350+Harduf-l@users.noreply.github.com>
2022-07-27 18:50:03 +0300

    making widget pies larger when necessary (#873)
    * passing size parameter to pieChartView component
    
    * added variant to footer, in widgets.style.services. option for no date range. footer gets information whether to show date range, or not
    
    * removing unnecessary import
    
    * fixes done

--- src/components/molecules/widgets/CountByTypePieWidget.tsx
+++ src/components/molecules/widgets/CountByTypePieWidget.tsx
@@ -4,11 +4,12 @@ import { IWidgetAccidentsByTypeData } from 'models/WidgetData';
 
 const ACCIDENT_TYPE = 'accident_type';
 const COUNT = 'count';
+const OUTER_RADIUS = '70%';
 
 interface IProps {
   data: IWidgetAccidentsByTypeData;
 }
-const CountByTypePieWidget: FC<IProps> = ({data}) => {
-  return <PieChartView data={data.items} xLabel={ACCIDENT_TYPE} yLabel={COUNT} />;
+const CountByTypePieWidget: FC<IProps> = ({ data }) => {
+  return <PieChartView data={data.items} outerRadius={OUTER_RADIUS} xLabel={ACCIDENT_TYPE} yLabel={COUNT} />;
 };
 export default CountByTypePieWidget;
