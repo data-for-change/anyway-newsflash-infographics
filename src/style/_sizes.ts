c5afdc1f0437a18cc6ace03a6a3e1d21d85c4770
Atalya Alon <atalya.alon@gmail.com>
2024-10-22 18:36:35 +0300

    revert card size, decrease content size

--- src/style/_sizes.ts
+++ src/style/_sizes.ts
@@ -1,8 +1,8 @@
 export const defaultBorderRadius = 4;
 export const sidBarWidth = 300;
 // card
-export const cardHeight = 430;
-export const cardWidth = 430;
+export const cardHeight = 500;
+export const cardWidth = 500;
 // card content and actions (footer)
 export const cardHeaderHeight = 70;
 export const cardFooterHeight = 50;
