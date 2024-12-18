760edf109c9ef45cf7b7314b06dfb40147f27d33
Atalya Alon <atalya.alon@gmail.com>
2024-12-18 13:50:36 +0200

    update AccidentCountByRoadLight as piechart

--- src/components/molecules/widgets/AccidentCountByRoadLight.tsx
+++ src/components/molecules/widgets/AccidentCountByRoadLight.tsx
@@ -1,12 +1,24 @@
 import React, { FC } from 'react';
 import { IWidgetAccidentCountByRoadLight } from 'models/WidgetData';
+import PieChartView from 'components/molecules/PieChartView';
 
+const ROAD_LIGHT = 'road_light';
+const COUNT = 'count';
+const INNER_RADIUS = '40%';
+const OUTER_RADIUS = '70%';
 interface IProps {
   data: IWidgetAccidentCountByRoadLight;
-  segmentText: string;
 }
 
-const AccidentCountByRoadLight: FC<IProps> = () => {
-  return <div> {} </div>;
+const AccidentCountByRoadLight: FC<IProps> = ({ data }) => {
+  return (
+    <PieChartView
+      data={data.items}
+      xLabel={ROAD_LIGHT}
+      yLabel={COUNT}
+      outerRadius={OUTER_RADIUS}
+      innerRadius={INNER_RADIUS}
+    />
+  );
 };
 export default AccidentCountByRoadLight;
