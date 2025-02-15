b3782bb89208bdbc832c94e116652e310a2016bc
Shaked Hayek <shaked.hayek@mail.huji.ac.il>
2025-02-15 21:20:09 +0200

    Fix bug in newsId fetch when different language url

--- src/utils/url.utils.ts
+++ src/utils/url.utils.ts
@@ -1,6 +1,6 @@
 export const getNewsIdFromUrl = () => {
   const pathname = window.location.pathname;
-  const [, , newsId] = pathname.split('/');
+  const newsId = pathname.split('/')[-1];
 
   return newsId ? { newsFlashId: +newsId } : { pageNumber: 1 };
 };
